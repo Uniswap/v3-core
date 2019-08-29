59d19b296294d6f8bb1878aa5ad13efcb8ccc048
haydenadams <haydenzadams@gmail.com>
2019-08-29 16:10:44 -0400

    finish initial contracts

---
+++ contracts/UniswapFactory.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.11;
+import "./UniswapExchange.sol";
+import "./interfaces/IUniswapExchange.sol";
+
+
+contract UniswapFactory {
+
+  event NewExchange(address indexed token, address indexed exchange);
+
+  uint256 public tokenCount;
+  mapping (address => address) public getExchange;
+  mapping (address => address) internal getToken;
+  mapping (uint256 => address) internal getTokenWithId;
+
+  function createExchange(address token) public returns (address) {
+    require(token != address(0));
+    require(getExchange[token] == address(0), 'EXCHANGE_EXISTS');
+    UniswapExchange exchange = new UniswapExchange(token);
+    exchange.setup(token);
+    getExchange[token] = address(exchange);
+    getToken[address(exchange)] = token;
+    uint256 tokenId = tokenCount + 1;
+    tokenCount = tokenId;
+    getTokenWithId[tokenId] = token;
+    emit NewExchange(token, address(exchange));
+    return address(exchange);
+  }
+}
---
+++ contracts/interfaces/IUniswapFactory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.11;
+
+interface IUniswapFactory {
+  event NewExchange(address indexed token, address indexed exchange);
+
+  function initializeFactory(address template) external;
+  function createExchange(address token) external returns (address payable);
+  function getExchange(address token) external view returns (address payable);
+  function getToken(address token) external view returns (address);
+  function getTokenWihId(uint256 token_id) external view returns (address);
+}
