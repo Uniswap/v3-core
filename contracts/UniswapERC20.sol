04b994380de7088d902ac78d338b96ff8655ea78
Dan Robinson <dan@paradigm.xyz>
2019-10-01 13:20:19 -0700

    contracts: track reserves in ERC20 and remove transferFrom

--- contracts/UniswapERC20.sol
+++ contracts/UniswapERC20.sol
@@ -10,6 +10,11 @@ contract UniswapERC20 is ERC20 {
   event AddLiquidity(address indexed provider, uint256 amountTokenA, uint256 amountTokenB);
   event RemoveLiquidity(address indexed provider, uint256 amountTokenA, uint256 amountTokenB);
 
+  struct Reserves {
+    uint128 reserveA;
+    uint128 reserveB;
+  }
+
   // ERC20 Data
   string public constant name = 'Uniswap V2';
   string public constant symbol = 'UNI-V2';
@@ -18,6 +23,7 @@ contract UniswapERC20 is ERC20 {
   address public tokenA;                // ERC20 token traded on this contract
   address public tokenB;                // ERC20 token traded on this contract
   address public factory;               // factory that created this contract
+  Reserves public reserves;             // cached reserve amounts
 
   bool private rentrancyLock = false;
 
@@ -48,25 +54,42 @@ contract UniswapERC20 is ERC20 {
     return numerator / denominator;
   }
 
-  function swap(address inputToken, address outputToken, uint256 amountSold, address recipient) private returns (uint256) {
-      uint256 inputReserve = IERC20(inputToken).balanceOf(address(this));
-      uint256 outputReserve = IERC20(outputToken).balanceOf(address(this));
-      uint256 amountBought = getInputPrice(amountSold, inputReserve, outputReserve);
-      require(IERC20(inputToken).transferFrom(msg.sender, address(this), amountSold));
-      require(IERC20(outputToken).transfer(recipient, amountBought));
-      return amountBought;
-  }
-
   //TO: DO msg.sender is wrapper
-  function swapAForB(uint256 amountSold, address recipient) public nonReentrant returns (uint256) {
-      uint256 amountBought = swap(tokenA, tokenB, amountSold, recipient);
+  function swapAForB(address recipient) public nonReentrant returns (uint256) {
+      address _tokenA = tokenA;
+      address _tokenB = tokenB;
+      Reserves memory _reserves = reserves;
+      uint256 newInputReserve = IERC20(_tokenA).balanceOf(address(this));
+      uint256 oldInputReserve = uint256(_reserves.reserveA);
+      uint256 oldOutputReserve = uint256(_reserves.reserveB);
+      uint256 amountSold = newInputReserve - oldInputReserve;
+      uint256 amountBought = getInputPrice(amountSold, oldInputReserve, oldOutputReserve);
+      require(IERC20(_tokenB).transfer(recipient, amountBought), "TRANSFER_FAILED");
+      uint256 newOutputReserve = IERC20(_tokenB).balanceOf(address(this));
+      reserves = Reserves({
+        reserveA: uint128(newInputReserve),
+        reserveB: uint128(newOutputReserve)
+      });
       emit SwapAForB(msg.sender, amountSold, amountBought);
       return amountBought;
   }
 
   //TO: DO msg.sender is wrapper
-  function swapBForA(uint256 amountSold, address recipient) public nonReentrant returns (uint256) {
-      uint256 amountBought = swap(tokenB, tokenA, amountSold, recipient);
+  function swapBForA(address recipient) public nonReentrant returns (uint256) {
+      address _tokenA = tokenA;
+      address _tokenB = tokenB;
+      Reserves memory _reserves = reserves;
+      uint256 newInputReserve = IERC20(_tokenB).balanceOf(address(this));
+      uint256 oldInputReserve = uint256(_reserves.reserveB);
+      uint256 oldOutputReserve = uint256(_reserves.reserveA);
+      uint256 amountSold = newInputReserve - oldInputReserve;
+      uint256 amountBought = getInputPrice(amountSold, oldInputReserve, oldOutputReserve);
+      require(IERC20(_tokenA).transfer(recipient, amountBought), "TRANSFER_FAILED");
+      uint256 newOutputReserve = IERC20(_tokenA).balanceOf(address(this));
+      reserves = Reserves({
+        reserveA: uint128(newOutputReserve),
+        reserveB: uint128(newInputReserve)
+      });
       emit SwapBForA(msg.sender, amountSold, amountBought);
       return amountBought;
   }
