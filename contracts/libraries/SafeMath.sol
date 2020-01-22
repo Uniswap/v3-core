585ee2ef824db671b71e351a91860a003c05431d
Noah Zinsmeister <noahwz@gmail.com>
2020-01-22 15:28:09 -0500

    tweak and add explanatory comments to libraries

--- contracts/libraries/SafeMath.sol
+++ contracts/libraries/SafeMath.sol
@@ -1,5 +1,7 @@
 pragma solidity =0.5.16;
 
+// a library for performing overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math)
+
 library SafeMath {
     function add(uint x, uint y) internal pure returns (uint z) {
         require((z = x + y) >= x, "ds-math-add-overflow");
--- contracts/libraries/UQ112x112.sol
+++ contracts/libraries/UQ112x112.sol
@@ -1,15 +1,20 @@
 pragma solidity =0.5.16;
 
+// a library for handling binary fixed point numbers (https://en.wikipedia.org/wiki/Q_(number_format))
+
+// range: [0, 2**112 - 1] (theoretical upper bound of 2**112 - (1 / 2**112) does not apply in this setting)
+// resolution: 1 / 2**112
+
 library UQ112x112 {
     uint224 constant Q112 = 2**112;
 
-    // encode a uint112 as a UQ112.112 fixed point number s.t. `y := z / 2**112`
+    // encode a uint112 as a UQ112x112
     function encode(uint112 y) internal pure returns (uint224 z) {
         z = uint224(y) * Q112; // never overflows
     }
 
-    // divide a UQ112.112 by a uint112 and return the result as a UQ112.112
-    function qdiv(uint224 x, uint112 y) internal pure returns (uint224 z) {
+    // divide a UQ112x112 by a uint112, returning a UQ112.112
+    function uqdiv(uint224 x, uint112 y) internal pure returns (uint224 z) {
         z = x / uint224(y);
     }
 }
