6344c31eebad9deeaa3f9d61cd0901d187172406
Noah Zinsmeister <noahwz@gmail.com>
2020-01-30 11:18:20 -0500

    fix typo

--- contracts/libraries/UQ112x112.sol
+++ contracts/libraries/UQ112x112.sol
@@ -13,7 +13,7 @@ library UQ112x112 {
         z = uint224(y) * Q112; // never overflows
     }
 
-    // divide a UQ112x112 by a uint112, returning a UQ112.112
+    // divide a UQ112x112 by a uint112, returning a UQ112x112
     function uqdiv(uint224 x, uint112 y) internal pure returns (uint224 z) {
         z = x / uint224(y);
     }
