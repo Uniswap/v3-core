7eb5c2963fd0617c778c378d6fd8c082d4da70a9
Anton Bukov <k06aaa@gmail.com>
2020-04-15 21:30:21 +0300

    Emit Approval event on allowance change

--- test/UniswapV2Factory.spec.ts
+++ test/UniswapV2Factory.spec.ts
@@ -68,7 +68,7 @@ describe('UniswapV2Factory', () => {
   it('createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES)
     const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(2512920)
+    expect(receipt.gasUsed).to.eq(2524962)
   })
 
   it('setFeeTo', async () => {
