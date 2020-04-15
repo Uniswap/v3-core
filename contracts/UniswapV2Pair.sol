4d977517d8171c5c57758966463f8e0d1aa7d094
Anton Bukov <k06aaa@gmail.com>
2020-04-15 21:23:42 +0300

    Optimization on Solidity constant

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -13,7 +13,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
     using UQ112x112 for uint224;
 
     uint public constant MINIMUM_LIQUIDITY = 10**3;
-    bytes4 private constant SELECTOR = bytes4(keccak256(bytes('transfer(address,uint256)')));
+    bytes4 private constant SELECTOR = 0xa9059cbb; // transfer(address,uint256)
 
     address public factory;
     address public token0;
@@ -59,6 +59,7 @@ contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
     event Sync(uint112 reserve0, uint112 reserve1);
 
     constructor() public {
+        require(SELECTOR == IERC20(0).transfer.selector);
         factory = msg.sender;
     }
 
