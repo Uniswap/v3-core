e03d3fadcae085c936c186937d28ad2049268b12
Moody Salem <moody.salem@gmail.com>
2020-04-17 16:16:47 -0400

    Fix the gas test and commit the combined build artifacts

--- test/UniswapV2Pair.spec.ts
+++ test/UniswapV2Pair.spec.ts
@@ -177,7 +177,7 @@ describe('UniswapV2Pair', () => {
     await mineBlock(provider, (await provider.getBlock('latest')).timestamp + 1)
     const tx = await pair.swap(expectedOutputAmount, 0, wallet.address, '0x', overrides)
     const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(73411)
+    expect(receipt.gasUsed).to.eq(73407)
   })
 
   it('burn', async () => {
