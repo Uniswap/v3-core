a21eaa2540bdc248429aea14ed8d55873e32ff7c
Moody Salem <moody.salem@gmail.com>
2020-04-19 20:02:36 -0500

    Add a few more test cases to TokenNamer.spec.ts

--- test/TokenNamer.spec.ts
+++ test/TokenNamer.spec.ts
@@ -15,6 +15,10 @@ const overrides = {
   gasLimit: 9999999
 }
 
+// last byte in bytes32 strings is null terminator
+const fullBytes32Name = 'NAME'.repeat(8).substr(0, 31)
+const fullBytes32Symbol = 'SYMB'.repeat(8).substr(0, 31)
+
 describe('TokenNamer', () => {
   const provider = new MockProvider({
     hardfork: 'istanbul',
@@ -70,12 +74,20 @@ describe('TokenNamer', () => {
       const token = await deployNoncompliant({ name: 'token name', symbol: 'tn' })
       expect(await getName(token.address)).to.eq('token name')
     })
+    it('works with noncompliant full bytes32', async () => {
+      const token = await deployNoncompliant({ name: fullBytes32Name, symbol: fullBytes32Symbol })
+      expect(await getName(token.address)).to.eq(fullBytes32Name)
+    })
     it('works with optional', async () => {
       const token = await deployOptional()
       expect(await getName(token.address)).to.eq(token.address.substr(2).toLowerCase())
     })
     it('works with non-code address', async () => {
       expect(await getName(AddressZero)).to.eq(AddressZero.substr(2))
+    })
+    it('works with really long strings', async () => {
+      const token = await deployCompliant({ name: 'token name'.repeat(32), symbol: 'tn'.repeat(32) })
+      expect(await getName(token.address)).to.eq('token name'.repeat(32))
     })
   })
 
@@ -88,12 +100,20 @@ describe('TokenNamer', () => {
       const token = await deployNoncompliant({ name: 'token name', symbol: 'tn' })
       expect(await getSymbol(token.address)).to.eq('tn')
     })
+    it('works with noncompliant full bytes32', async () => {
+      const token = await deployNoncompliant({ name: fullBytes32Name, symbol: fullBytes32Symbol })
+      expect(await getSymbol(token.address)).to.eq(fullBytes32Symbol)
+    })
     it('works with optional', async () => {
       const token = await deployOptional()
       expect(await getSymbol(token.address)).to.eq(token.address.substr(2, 6).toLowerCase())
     })
     it('works with non-code address', async () => {
       expect(await getSymbol(AddressZero)).to.eq(AddressZero.substr(2, 6))
+    })
+    it('works with really long strings', async () => {
+      const token = await deployCompliant({ name: 'token name'.repeat(32), symbol: 'tn'.repeat(32) })
+      expect(await getSymbol(token.address)).to.eq('tn'.repeat(32))
     })
   })
 })
