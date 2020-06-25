aba5791b9faf4f1f7cfa8617eef2ef36147008fb
Moody Salem <moody.salem@gmail.com>
2020-06-25 10:55:32 -0500

    add a verification test

--- test/PriceMath.spec.ts
+++ test/PriceMath.spec.ts
@@ -1,8 +1,10 @@
 import chai, { expect } from 'chai'
 import { Contract } from 'ethers'
+import { bigNumberify } from 'ethers/utils'
 import { solidity, MockProvider, deployContract } from 'ethereum-waffle'
 
 import PriceMathTest from '../build/PriceMathTest.json'
+import { expandTo18Decimals } from './shared/utilities'
 
 chai.use(solidity)
 
@@ -21,9 +23,57 @@ describe('UniswapV3ERC20', () => {
 
   describe('#getTradeToRatio', () => {
     describe('edge cases', () => {
-      it('0 reserves', async () => {
-        await priceMath.getTradeToRatio(0, 0, 0, [0])
+      it('0 all', async () => {
+        expect(await priceMath.getTradeToRatio(0, 0, 0, [0])).to.eq(0)
       })
+    })
+
+    describe('1:100 to 1:50 at 30bps', () => {
+      it('returns 415459942199694131', async () => {
+        expect(
+          await priceMath.getTradeToRatio(expandTo18Decimals(1), expandTo18Decimals(100), 3000, [
+            expandTo18Decimals(1)
+              .mul(bigNumberify(2).pow(112))
+              .div(expandTo18Decimals(50))
+          ])
+        ).to.eq('415459942199694131')
+      })
+      it('verify result', () => {
+        const amountWithoutFee = bigNumberify('415459942199694131')
+          .mul(997)
+          .div(1000)
+        const reserveInAfter = expandTo18Decimals(1).add(amountWithoutFee)
+        const reserveOutAfter = expandTo18Decimals(1)
+          .mul(expandTo18Decimals(100))
+          .div(reserveInAfter)
+        const ratioAfter = reserveInAfter.mul(bigNumberify(2).pow(112)).div(reserveOutAfter)
+
+        const targetRatio = expandTo18Decimals(1)
+          .mul(bigNumberify(2).pow(112))
+          .div(expandTo18Decimals(50))
+        // a difference of lte 2^56 in a uq112x112 is <= 2^-56
+        expect(ratioAfter.sub(targetRatio).abs()).to.be.lte(bigNumberify(2).pow(56))
+      })
+    })
+
+    it('1:100 to 1:50 at 60bps', async () => {
+      expect(
+        await priceMath.getTradeToRatio(expandTo18Decimals(1), expandTo18Decimals(100), 6000, [
+          expandTo18Decimals(1)
+            .mul(bigNumberify(2).pow(112))
+            .div(expandTo18Decimals(50))
+        ])
+      ).to.eq('416713845445769666')
+    })
+
+    it('1:100 to 1:75 at 45bps', async () => {
+      expect(
+        await priceMath.getTradeToRatio(expandTo18Decimals(1), expandTo18Decimals(100), 4500, [
+          expandTo18Decimals(1)
+            .mul(bigNumberify(2).pow(112))
+            .div(expandTo18Decimals(75))
+        ])
+      ).to.eq('155399837648670546')
     })
   })
 })
