4e8389f52b9bde53b50fc45ebc04578cbe91db98
Noah Zinsmeister <noahwz@gmail.com>
2020-07-06 16:53:44 -0400

    refactor for readability

--- contracts/UniswapV3Pair.sol
+++ contracts/UniswapV3Pair.sol
@@ -28,17 +28,13 @@ contract UniswapV3Pair is IUniswapV3Pair {
     using FixedPointExtra for FixedPoint.uq112x112;
     using FixedPoint for FixedPoint.uq144x112;
 
-    uint112 public constant override MINIMUM_LIQUIDITY = uint112(10**3);
-    int16 public constant MAX_TICK = type(int16).max;
-    int16 public constant MIN_TICK = type(int16).min;
+    uint112 public constant override MINIMUM_LIQUIDITY = 10**3;
     uint16 public constant MAX_FEEVOTE = 6000; // 60 bps
-    uint16 public constant MIN_FEEVOTE = 0; // 0 bps
+    uint16 public constant MIN_FEEVOTE =    0; // 0 bps
 
     address public immutable override factory;
     address public immutable override token0;
     address public immutable override token1;
-
-    uint112 public totalFeeVote;
 
     uint112 private reserve0;           // uses single storage slot, accessible via getReserves
     uint112 private reserve1;           // uses single storage slot, accessible via getReserves
@@ -48,28 +44,29 @@ contract UniswapV3Pair is IUniswapV3Pair {
     uint public override price1CumulativeLast;
     uint public override kLast; // reserve0 * reserve1, as of immediately after the most recent liquidity event
 
-    uint112 private virtualSupply;  // current virtual supply;
+    uint112 public virtualSupply;  // current virtual supply
 
-    int16 public currentTick; // the current tick for the token0 price (rounded down)
-
-    uint private unlocked = 1;
+    int16 public currentTick; // the current tick for the token0 price (i.e. token1/token0), rounded down
     
     // these only have relative meaning, not absolute—their value depends on when the tick is initialized
     struct TickInfo {
-        uint32 secondsGrowthOutside;         // measures number of seconds spent while pool was on other side of this tick (from the current price)
-        FixedPoint.uq112x112 kGrowthOutside; // measures growth due to fees while pool was on the other side of this tick (from the current price)
+        // seconds spent while pool was on other side of this tick (from the current price)
+        uint32 secondsGrowthOutside;
+        // growth due to fees while pool was on the other side of this tick (from the current price)
+        FixedPoint.uq112x112 kGrowthOutside;
     }
+    mapping (int16 => TickInfo) tickInfos; // mapping from tick indexes to information about that tick
+    mapping (int16 => int112) deltas;      // mapping from tick indexes to amount of token0 kicked in or out when tick is crossed going from left to right (token0 price going up)
 
-    mapping (int16 => TickInfo) tickInfos;  // mapping from tick indexes to information about that tick
-    mapping (int16 => int112) deltas;       // mapping from tick indexes to amount of token0 kicked in or out when tick is crossed going from left to right (token0 price going up)
-
+    uint112 public totalFeeVote;
     Aggregate aggregateFeeVote;
-    mapping (int16 => Aggregate) deltaFeeVotes;       // mapping from tick indexes to amount of token0 kicked in or out when tick is crossed
+    mapping (int16 => Aggregate) deltaFeeVotes; // mapping from tick indexes to amount of token0 kicked in or out when tick is crossed
 
     // TODO: is this really the best way to map (address, int16, int16) to position
     // keccak256(abi.encodePacked(address, int16, int16))
     mapping (bytes32 => Position) positions;
 
+    uint private unlocked = 1;
     modifier lock() {
         require(unlocked == 1, 'UniswapV3: LOCKED');
         unlocked = 0;
@@ -77,55 +74,85 @@ contract UniswapV3Pair is IUniswapV3Pair {
         unlocked = 1;
     }
 
-    // returns sqrt(x*y)/shares
-    function getInvariant() public view returns (FixedPoint.uq112x112 memory k) {
-        uint112 rootXY = uint112(Babylonian.sqrt(uint256(reserve0) * uint256(reserve1)));
-        return FixedPoint.encode(rootXY).div(virtualSupply);
-    }
-
-    function getGrowthAbove(int16 tickIndex, int16 _currentTick, FixedPoint.uq112x112 memory _k) public view returns (FixedPoint.uq112x112 memory) {
-        FixedPoint.uq112x112 memory kGrowthOutside = tickInfos[tickIndex].kGrowthOutside;
-        if (_currentTick >= tickIndex) {
-            // this range is currently active
-            return _k.uqdiv112(kGrowthOutside);
-        } else {
-            // this range is currently inactive
-            return kGrowthOutside;
-        }
-    }
-
-    function getGrowthBelow(int16 tickIndex, int16 _currentTick, FixedPoint.uq112x112 memory _k) public view returns (FixedPoint.uq112x112 memory) {
-        FixedPoint.uq112x112 memory kGrowthOutside = tickInfos[tickIndex].kGrowthOutside;
-        if (_currentTick < tickIndex) {
-            // this range is currently active
-            return _k.uqdiv112(kGrowthOutside);
-        } else {
-            // this range is currently inactive
-            return kGrowthOutside;
-        }
-    }
-
-    // gets the growth in K for within a particular range
-    // this only has relative meaning, not absolute
-    function getGrowthInside(int16 _lowerTick, int16 _upperTick) public view returns (FixedPoint.uq112x112 memory growth) {
-        // TODO: simpler or more precise way to compute this?
-        FixedPoint.uq112x112 memory _k = getInvariant();
-        int16 _currentTick = currentTick;
-        FixedPoint.uq112x112 memory growthAbove = getGrowthAbove(_upperTick, _currentTick, _k);
-        FixedPoint.uq112x112 memory growthBelow = getGrowthBelow(_lowerTick, _currentTick, _k);
-        return growthAbove.uqmul112(growthBelow).reciprocal().uqmul112(_k);
-    }
-
-    function getReserves() public override view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) {
+    function getReserves()
+        public
+        override
+        view
+        returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast)
+    {
         _reserve0 = reserve0;
         _reserve1 = reserve1;
         _blockTimestampLast = blockTimestampLast;
     }
 
-    constructor(address token0_, address token1_) public {
+    // returns sqrt(reserve0 * reserve1) / virtual supply
+    function getRootKOverShares() public view returns (FixedPoint.uq112x112 memory rootKOverShares) {
+        uint rootK = Babylonian.sqrt(uint(reserve0) * reserve1);
+        rootKOverShares = FixedPoint.encode(uint112(rootK)).div(virtualSupply);
+    }
+
+    function getGrowthBelow(int16 tickIndex, FixedPoint.uq112x112 memory rootKOverShares)
+        public
+        view
+        returns (FixedPoint.uq112x112 memory growthBelow)
+    {
+        growthBelow = tickInfos[tickIndex].kGrowthOutside;
+        // the range is currently active
+        if (currentTick < tickIndex) {
+            growthBelow = rootKOverShares.uqdiv112(growthBelow);
+        }
+    }
+
+    function getGrowthAbove(int16 tickIndex, FixedPoint.uq112x112 memory rootKOverShares)
+        public
+        view
+        returns (FixedPoint.uq112x112 memory growthAbove)
+    {
+        growthAbove = tickInfos[tickIndex].kGrowthOutside;
+        // this range is currently active
+        if (currentTick >= tickIndex) {
+            return rootKOverShares.uqdiv112(growthAbove);
+        }
+    }
+
+    // gets the growth in rootKOverShares within a particular range
+    // this only has relative meaning, not absolute
+    // TODO: simpler or more precise way to compute this?
+    function getGrowthInside(int16 lowerTick, int16 upperTick)
+        public
+        view
+        returns (FixedPoint.uq112x112 memory growth)
+    {
+        FixedPoint.uq112x112 memory rootKOverShares = getRootKOverShares();
+        FixedPoint.uq112x112 memory growthBelow = getGrowthBelow(lowerTick, rootKOverShares);
+        FixedPoint.uq112x112 memory growthAbove = getGrowthAbove(upperTick, rootKOverShares);
+        growth = growthAbove.uqmul112(growthBelow).reciprocal().uqmul112(rootKOverShares);
+    }
+
+    // given an amount of liquidity and a price, return the value of that liquidity at the price
+    function getBalancesAtPrice(int112 liquidity, FixedPoint.uq112x112 memory price) 
+        private
+        pure
+        returns (int112 balance0, int112 balance1)
+    {
+        balance0 = price.reciprocal().sqrt().smul112(liquidity);
+        balance1 = price.smul112(balance0);
+    }
+
+    // given an amount of liquidity and a tick, return the value of that liquidity at the price determined by the tick
+    function getBalancesAtTick(int112 liquidity, int16 tick) private pure returns (int112 balance0, int112 balance1) {
+        // this is a lie - one of them should be near infinite
+        // TODO ensure this is safe and the right thing to do
+        if (tick == TickMath.MIN_TICK || tick == TickMath.MAX_TICK) {
+            return (0, 0);
+        }
+        return getBalancesAtPrice(liquidity, TickMath.getPrice(tick));
+    }
+
+    constructor(address _token0, address _token1) public {
         factory = msg.sender;
-        token0 = token0_;
-        token1 = token1_;
+        token0 = _token0;
+        token1 = _token1;
     }
 
     // update reserves and, on the first call per block, price accumulators
@@ -150,14 +177,14 @@ contract UniswapV3Pair is IUniswapV3Pair {
         uint112 _virtualSupply = virtualSupply;
         if (feeOn) {
             if (_kLast != 0) {
-                uint rootK = Babylonian.sqrt(uint(_reserve0).mul(_reserve1));
+                uint rootK = Babylonian.sqrt(uint(_reserve0) * _reserve1);
                 uint rootKLast = Babylonian.sqrt(_kLast);
                 if (rootK > rootKLast) {
                     uint numerator = uint(_virtualSupply).mul(rootK.sub(rootKLast));
                     uint denominator = rootK.mul(5).add(rootKLast);
                     uint112 liquidity = uint112(numerator / denominator);
                     if (liquidity > 0) {
-                        positions[keccak256(abi.encodePacked(feeTo, int16(0), int16(0)))].liquidity += liquidity;
+                        positions[getPositionKey(feeTo, 0, 0)].liquidity += liquidity;
                         virtualSupply = _virtualSupply + liquidity;
                     }
                 }
@@ -167,33 +194,19 @@ contract UniswapV3Pair is IUniswapV3Pair {
         }
     }
 
-    function getBalancesAtPrice(int112 liquidity, FixedPoint.uq112x112 memory price) internal pure returns (int112 balance0, int112 balance1) {
-        balance0 = price.reciprocal().sqrt().smul112(liquidity);
-        balance1 = price.smul112(balance0);
-    }
-
-    function getBalancesAtTick(int112 liquidity, int16 tick) internal pure returns (int112 balance0, int112 balance1) {
-        if (tick == MIN_TICK || tick == MAX_TICK) {
-            // this is a lie—one of them should be near infinite—but I think an innocuous lie since that one is never checked
-            return (0, 0);
-        }
-        FixedPoint.uq112x112 memory price = TickMath.getPrice(tick);
-        return getBalancesAtPrice(liquidity, price);
-    }
-
     function initialAdd(uint112 amount0, uint112 amount1, int16 startingTick, uint16 feeVote) external override lock returns (uint112 liquidity) {
         require(virtualSupply == 0, "UniswapV3: ALREADY_INITIALIZED");
         require(feeVote >= MIN_FEEVOTE && feeVote <= MAX_FEEVOTE, "UniswapV3: INVALID_FEE_VOTE");
         FixedPoint.uq112x112 memory price = FixedPoint.encode(amount1).div(amount0);
         require(price._x > TickMath.getPrice(startingTick)._x && price._x < TickMath.getPrice(startingTick + 1)._x);
         bool feeOn = _mintFee(0, 0);
-        liquidity = uint112(Babylonian.sqrt(uint256(amount0).mul(uint256(amount1))).sub(MINIMUM_LIQUIDITY));
-        positions[keccak256(abi.encodePacked(address(0), MIN_TICK, MAX_TICK))] = Position({
+        liquidity = uint112(Babylonian.sqrt(uint(amount0) * amount1).sub(MINIMUM_LIQUIDITY));
+        positions[keccak256(abi.encodePacked(address(0), TickMath.MIN_TICK, TickMath.MAX_TICK))] = Position({
             liquidity: MINIMUM_LIQUIDITY,
             lastNormalizedLiquidity: MINIMUM_LIQUIDITY,
             feeVote: feeVote
         });
-        positions[keccak256(abi.encodePacked(msg.sender, MIN_TICK, MAX_TICK))] = Position({
+        positions[keccak256(abi.encodePacked(msg.sender, TickMath.MIN_TICK, TickMath.MAX_TICK))] = Position({
             liquidity: liquidity,
             lastNormalizedLiquidity: liquidity,
             feeVote: feeVote
@@ -211,12 +224,12 @@ contract UniswapV3Pair is IUniswapV3Pair {
         TransferHelper.safeTransferFrom(token0, msg.sender, address(this), amount0);
         TransferHelper.safeTransferFrom(token1, msg.sender, address(this), amount1);
         if (feeOn) kLast = uint(_reserve0).mul(_reserve1);
-        emit SetPosition(address(0), int112(MINIMUM_LIQUIDITY), MIN_TICK, MAX_TICK, feeVote);
-        emit SetPosition(msg.sender, int112(liquidity), MIN_TICK, MAX_TICK, feeVote);
+        emit SetPosition(address(0), int112(MINIMUM_LIQUIDITY), TickMath.MIN_TICK, TickMath.MAX_TICK, feeVote);
+        emit SetPosition(msg.sender, int112(liquidity), TickMath.MIN_TICK, TickMath.MAX_TICK, feeVote);
     }
 
     // called when adding or removing liquidity that is within range
-    function updateVirtualLiquidity(int112 liquidity, Aggregate memory deltaFeeVote) internal returns (int112 virtualAmount0, int112 virtualAmount1) {
+    function updateVirtualLiquidity(int112 liquidity, Aggregate memory deltaFeeVote) private returns (int112 virtualAmount0, int112 virtualAmount1) {
         (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
         (virtualAmount0, virtualAmount1) = getBalancesAtPrice(liquidity, FixedPoint.encode(reserve1).div(reserve0));
         bool feeOn = _mintFee(_reserve0, _reserve1);
@@ -230,13 +243,13 @@ contract UniswapV3Pair is IUniswapV3Pair {
         aggregateFeeVote = aggregateFeeVote.add(deltaFeeVote);
     }
 
-    function _initializeTick(int16 tickIndex, int16 _currentTick) internal {
+    function _initializeTick(int16 tickIndex) private {
         if (tickInfos[tickIndex].kGrowthOutside._x == 0) {
             // by convention, we assume that all growth before tick was initialized happened _below_ a tick
-            if (tickIndex <= _currentTick) {
+            if (tickIndex <= currentTick) {
                 tickInfos[tickIndex] = TickInfo({
                     secondsGrowthOutside: uint32(now % 2**32),
-                    kGrowthOutside: getInvariant()
+                    kGrowthOutside: getRootKOverShares()
                 });
             } else {
                 tickInfos[tickIndex] = TickInfo({
@@ -245,6 +258,14 @@ contract UniswapV3Pair is IUniswapV3Pair {
                 });
             }
         }
+    }
+
+    function getPositionKey(address owner, int16 lowerTick, int16 upperTick)
+        private
+        pure
+        returns (bytes32 positionKey)
+    {
+        positionKey = keccak256(abi.encodePacked(owner, lowerTick, upperTick));
     }
 
     // add or remove a specified amount of virtual liquidity from a specified range, and/or change feeVote for that range
@@ -257,27 +278,27 @@ contract UniswapV3Pair is IUniswapV3Pair {
         require(feeVote > MIN_FEEVOTE && feeVote < MAX_FEEVOTE, "UniswapV3: INVALID_FEE_VOTE");
         require(lowerTick < upperTick, "UniswapV3: BAD_TICKS");
         { // scope to help with compilation
-        bytes32 positionKey = keccak256(abi.encodePacked(msg.sender, lowerTick, upperTick));
         require(virtualSupply > 0, 'UniswapV3: NOT_INITIALIZED');
-        Position memory _position = positions[positionKey];
+        bytes32 positionKey = getPositionKey(msg.sender, lowerTick, upperTick);
+        Position storage position = positions[positionKey];
         // initialize tickInfos if they don't exist yet
-        _initializeTick(lowerTick, currentTick);
-        _initializeTick(upperTick, currentTick);
+        _initializeTick(lowerTick);
+        _initializeTick(upperTick);
         // before moving on, rebate any collected fees to user
         // note that unlevered liquidity wrapper can automatically recompound by setting liquidityDelta to their accumulated fees
         FixedPoint.uq112x112 memory kGrowthInside = getGrowthInside(lowerTick, upperTick);
         { // scope to help with compilation
-        int112 feeLiquidity = kGrowthInside.smul112(int112(_position.lastNormalizedLiquidity)) - int112(_position.liquidity);
+        int112 feeLiquidity = kGrowthInside.smul112(int112(position.lastNormalizedLiquidity)) - int112(position.liquidity);
         (amount0, amount1) = getBalancesAtPrice(-feeLiquidity, FixedPoint.encode(reserve1).div(reserve0));
         }
         // update position
         Position memory newPosition = Position({
-            liquidity: _position.liquidity.sadd(liquidityDelta),
-            lastNormalizedLiquidity: kGrowthInside.reciprocal().mul112(_position.liquidity.sadd(liquidityDelta)).decode(),
+            liquidity: position.liquidity.sadd(liquidityDelta),
+            lastNormalizedLiquidity: kGrowthInside.reciprocal().mul112(position.liquidity.sadd(liquidityDelta)).decode(),
             feeVote: feeVote
         });
+        deltaFeeVote = newPosition.totalFeeVote().sub(position.totalFeeVote());
         positions[positionKey] = newPosition;
-        deltaFeeVote = newPosition.totalFeeVote().sub(_position.totalFeeVote());
         }
         // calculate how much the newly added/removed shares are worth at lower ticks and upper ticks
         (int112 lowerToken0Balance, int112 lowerToken1Balance) = getBalancesAtTick(liquidityDelta, lowerTick);
@@ -309,8 +330,6 @@ contract UniswapV3Pair is IUniswapV3Pair {
         }
         emit SetPosition(msg.sender, liquidityDelta, lowerTick, upperTick, feeVote);
     }
-
-
 
     // TODO: implement swap1for0, or integrate it into this
     function swap0for1(uint amountIn, address to, bytes calldata data) external lock {
@@ -355,7 +374,7 @@ contract UniswapV3Pair is IUniswapV3Pair {
                 tickInfos[_currentTick] = TickInfo({
                     // overflow is desired
                     secondsGrowthOutside: uint32(block.timestamp % 2**32) - _oldTickInfo.secondsGrowthOutside,
-                    kGrowthOutside: getInvariant().uqdiv112(_oldKGrowthOutside)
+                    kGrowthOutside: getRootKOverShares().uqdiv112(_oldKGrowthOutside)
                 });
                 _currentTick -= 1;
                 if (feeOn) kLast = uint(_reserve0).mul(_reserve1);
