f3d5340b11db781434eb828aebd069f596b85e1c
Moody Salem <moody.salem@gmail.com>
2020-08-20 11:47:32 -0500

    fixes #37, more accurate divuq, muluq and some unit tests

--- contracts/libraries/FixedPointExtra.sol
+++ contracts/libraries/FixedPointExtra.sol
@@ -14,15 +14,29 @@ library FixedPointExtra {
         return other < 0 ? -int(z) : z;
     }
 
+    // lower 112 bits, representing decimal portion of the number, i.e. 14 bytes
+    uint224 public constant LOWER_MASK = 0xffff_ffff_ffff_ffff_ffff_ffff_ffff;
+
     // multiply a UQ112x112 by a UQ112x112, returning a UQ112x112
     function muluq(FixedPoint.uq112x112 memory self, FixedPoint.uq112x112 memory other)
         internal
         pure
         returns (FixedPoint.uq112x112 memory)
     {
-        uint z = uint(self._x >> 96) * (other._x >> 96);
-        require(z <= type(uint144).max, "FixedPointExtra: MULTIPLICATION_OVERFLOW");
-        return FixedPoint.uq112x112(uint224(z << 80));
+        uint224 upper_self = self._x >> 112; // * 2^0
+        uint224 lower_self = self._x & LOWER_MASK; // * 2^-112
+        uint224 upper_other = other._x >> 112; // * 2^0
+        uint224 lower_other = other._x & LOWER_MASK; // * 2^-112
+
+        // partial products
+        uint224 uppers = upper_self * upper_other; // * 2^0
+        uint224 lowers = lower_self * lower_other; // * 2^-224
+        uint224 uppers_lowero = upper_self * lower_other; // * 2^-112
+        uint224 uppero_lowers = upper_other * lower_self; // * 2^-112
+
+        uint sum = uint(uppers << 112) + uppers_lowero + uppero_lowers + (lowers >> 112);
+        require(sum <= uint224(-1), "FixedPointExtra: MULTIPLICATION_OVERFLOW");
+        return FixedPoint.uq112x112(uint224(sum));
     }
 
     // divide a UQ112x112 by a UQ112x112, returning a UQ112x112
---
+++ test/FixedPointExtra.spec.ts
@@ -0,0 +1,65 @@
+import chai, { expect } from 'chai'
+import { solidity, MockProvider, deployContract } from 'ethereum-waffle'
+import { Contract, BigNumber } from 'ethers'
+
+import FixedPointExtraTest from '../build/FixedPointExtraTest.json'
+
+chai.use(solidity)
+
+const overrides = {
+  gasLimit: 9999999
+}
+
+const Q112 = BigNumber.from(2).pow(112)
+
+describe.only('FixedPointExtra', () => {
+  const provider = new MockProvider({
+    ganacheOptions: {
+      hardfork: 'istanbul',
+      mnemonic: 'horn horn horn horn horn horn horn horn horn horn horn horn',
+      gasLimit: 9999999
+    }
+  })
+  const [wallet] = provider.getWallets()
+
+  let fixedPointExtra: Contract
+  before('deploy FixedPointExtraTest', async () => {
+    fixedPointExtra = await deployContract(wallet, FixedPointExtraTest, [], overrides)
+  })
+
+  describe('#muluq', () => {
+    it('1 x 1 == 1', async () => {
+      expect((await fixedPointExtra.muluq([Q112], [Q112]))[0]).to.eq(Q112)
+    })
+
+    it('3.5 x 2.2 == 7.7', async () => {
+      expect((await fixedPointExtra.muluq([Q112.mul(35).div(10)], [Q112.mul(22).div(10)]))[0]).to.eq(
+        Q112.mul(77)
+          .div(10)
+          .sub(1) // off by 1 * 2^-112
+      )
+    })
+
+    it('gas', async () => {
+      expect(await fixedPointExtra.muluqGasUsed([Q112.mul(35).div(10)], [Q112.mul(22).div(10)])).to.eq('446')
+    })
+  })
+
+  describe('#divuq', () => {
+    it('1 / 1 == 1', async () => {
+      expect((await fixedPointExtra.divuq([Q112], [Q112]))[0]).to.eq(Q112)
+    })
+
+    it('3.5 / 2.2 == ~1.5909090909', async () => {
+      expect((await fixedPointExtra.divuq([Q112.mul(35).div(10)], [Q112.mul(22).div(10)]))[0]).to.eq(
+        Q112.mul(35)
+          .div(22)
+          .sub(3) // off by 3 * 2^-112
+      )
+    })
+
+    it('gas', async () => {
+      expect(await fixedPointExtra.divuqGasUsed([Q112.mul(35).div(10)], [Q112.mul(22).div(10)])).to.eq('800')
+    })
+  })
+})
