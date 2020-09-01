d28c90f17e65f4781f72b4244a4d9a73b9bb7ddc
Noah Zinsmeister <noahwz@gmail.com>
2020-09-01 10:25:07 -0400

    fix logic

--- contracts/UniswapV3Pair.sol
+++ contracts/UniswapV3Pair.sol
@@ -360,18 +360,20 @@ contract UniswapV3Pair is IUniswapV3Pair {
                 if (liquidityProtocol > 0) {
                     // TODO figure out how we want to actually issue liquidityProtocol to feeTo
                     liquidityFee -= liquidityProtocol;
-                    // TODO figure out if this is correct
-                    // accrue existing feeTo position, and add liquidityProtocol
                     Position storage positionProtocol =
                         _getPosition(feeTo, TickMath.MIN_TICK, TickMath.MAX_TICK, feeVote);
                     FixedPoint.uq112x112 memory g = getG();
-                    positionProtocol.liquidity =
-                        uint(FixedPoint.decode144(g.mul(positionProtocol.liquidityAdjusted)))
-                        .sub(positionProtocol.liquidity)
+                    // TODO figure out if this is correct
+                    // accrue existing feeTo position (if possible) and add new protocol liquidity
+                    positionProtocol.liquidity = uint(
+                        FixedPoint.decode144(g.mul(positionProtocol.liquidityAdjusted)) > positionProtocol.liquidity ?
+                        FixedPoint.decode144(g.mul(positionProtocol.liquidityAdjusted)) :
+                        positionProtocol.liquidity
+                    )
                         .add(liquidityProtocol)
                         .toUint112();
                     positionProtocol.liquidityAdjusted =
-                        uint(FixedPoint.decode144(g.reciprocal().mul(position.liquidity))).toUint112();
+                        uint(FixedPoint.encode(positionProtocol.liquidity)._x / g._x).toUint112();
                 }
             }
 
