872d8f19d71ee810db40a018195bcb48d2b46381
Moody Salem <moody.salem@gmail.com>
2020-10-19 16:10:44 -0500

    remove todos about gas (i checked)

--- contracts/libraries/SafeCast.sol
+++ contracts/libraries/SafeCast.sol
@@ -3,17 +3,14 @@ pragma solidity >=0.5.0;
 
 // contains methods for safely casting between integer types
 library SafeCast {
-    // TODO check that this is gas efficient as compared to requiring `y <= type(uint112).max`
     function toUint112(uint256 y) internal pure returns (uint112 z) {
         require((z = uint112(y)) == y, 'SafeCast::toUint112: downcast overflow');
     }
 
-    // TODO check that this is gas efficient as compared to requiring `y <= type(int112).max`
     function toInt112(uint256 y) internal pure returns (int112 z) {
         require((z = int112(y)) >= 0 && uint256(z) == y, 'SafeCast::toInt112: downcast overflow');
     }
 
-    // TODO check that this is gas efficient as compared to requiring `y <= type(int128).max`
     function itoInt112(int256 y) internal pure returns (int112 z) {
         require((z = int112(y)) == y, 'SafeCast::itoInt112: downcast overflow');
     }
