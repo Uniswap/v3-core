47add64f844c38c492c7b6ff0fdd55d9182966cb
Moody Salem <moody.salem@gmail.com>
2020-10-23 15:45:53 -0500

    try to handle more cases with safemath

--- contracts/test/PriceMathEchidnaTest.sol
+++ contracts/test/PriceMathEchidnaTest.sol
@@ -2,10 +2,13 @@
 pragma solidity =0.6.12;
 
 import '@uniswap/lib/contracts/libraries/FixedPoint.sol';
+import '@openzeppelin/contracts/math/SafeMath.sol';
 
 import '../libraries/PriceMath.sol';
 
 contract PriceMathEchidnaTest {
+    using SafeMath for uint256;
+
     function getInputToRatioAlwaysExceedsNextPrice(
         uint112 reserveIn,
         uint112 reserveOut,
@@ -15,11 +18,15 @@ contract PriceMathEchidnaTest {
         require(reserveIn > 1001 && reserveOut > 1001 && lpFee < PriceMath.LP_FEE_BASE);
 
         uint112 amountIn = PriceMath.getInputToRatio(reserveIn, reserveOut, lpFee, FixedPoint.uq112x112(inOutRatio));
+        uint256 amountInLessFee = (uint256(amountIn).mul(PriceMath.LP_FEE_BASE - lpFee)).div(PriceMath.LP_FEE_BASE);
+        if (amountInLessFee == 0) {
+            assert((uint256(reserveIn) << 112) / reserveOut >= inOutRatio);
+            return;
+        }
 
-        uint256 amountInLessFee = (uint256(amountIn) * (PriceMath.LP_FEE_BASE - lpFee)) / PriceMath.LP_FEE_BASE;
-        uint256 amountOut = reserveOut - ((uint256(reserveIn) * reserveOut) / (uint256(reserveIn) + amountInLessFee));
-        if (amountOut >= reserveOut) return;
+        uint256 reserveOutAfter = (uint256(reserveIn).mul(reserveOut)) / (uint256(reserveIn).add(amountInLessFee));
+        require(reserveOutAfter > 0);
 
-        assert(((uint256(reserveIn) + amountIn) << 112) / (reserveOut - amountOut) >= inOutRatio);
+        assert(((uint256(reserveIn).add(amountIn)) << 112) / reserveOutAfter >= inOutRatio);
     }
 }
