980158ceed439a9079d6a73bcc829735d0cd92ae
Noah Zinsmeister <noahwz@gmail.com>
2020-10-23 11:36:02 -0400

    add price asserts
    use min

--- contracts/UniswapV3Pair.sol
+++ contracts/UniswapV3Pair.sol
@@ -357,6 +357,14 @@ contract UniswapV3Pair is IUniswapV3Pair {
         reserve0Virtual = reserve0Virtual.addi(amount0).toUint112();
         reserve1Virtual = reserve1Virtual.addi(amount1).toUint112();
 
+        // TODO remove this eventually, it's simply meant to show the direction of rounding in both cases
+        FixedPoint.uq112x112 memory priceNext = FixedPoint.fraction(reserve1Virtual, reserve0Virtual);
+        if (amount0 > 0) {
+            assert(priceNext._x >= price._x);
+        } else {
+            assert(priceNext._x <= price._x);
+        }
+
         require(reserve0Virtual >= TOKEN_MIN, 'UniswapV3: RESERVE_0_TOO_SMALL');
         require(reserve1Virtual >= TOKEN_MIN, 'UniswapV3: RESERVE_1_TOO_SMALL');
 
@@ -367,14 +375,6 @@ contract UniswapV3Pair is IUniswapV3Pair {
         virtualSupplies[feeVote] = virtualSupplies[feeVote]
             .addi(((int256(rootK) - rootKLast) * virtualSupply) / rootKLast)
             .toUint112();
-
-        // TODO remove this eventually, it's simply meant to show the direction of rounding in both cases
-        FixedPoint.uq112x112 memory priceNext = FixedPoint.fraction(reserve1Virtual, reserve0Virtual);
-        if (amount0 > 0) {
-            assert(priceNext._x >= price._x);
-        } else {
-            assert(priceNext._x <= price._x);
-        }
     }
 
     // add or remove a specified amount of liquidity from a specified range, and/or change feeVote for that range
@@ -590,7 +590,7 @@ contract UniswapV3Pair is IUniswapV3Pair {
                     ? FullMath.mulDiv(reserveInVirtualNext, step.nextPrice._x, uint256(1) << 112)
                     : FixedPoint.encode(reserveInVirtualNext)._x / step.nextPrice._x;
                 uint112 amountOutMaximum = reserveOutVirtual.sub(reserveOutVirtualNext).toUint112();
-                step.amountOut = step.amountOut > amountOutMaximum ? amountOutMaximum : step.amountOut;
+                step.amountOut = Math.min(step.amountOut, amountOutMaximum).toUint112();
 
                 if (params.zeroForOne) {
                     reserve0Virtual = (uint256(reserve0Virtual) + step.amountIn).toUint112();
@@ -599,6 +599,17 @@ contract UniswapV3Pair is IUniswapV3Pair {
                     reserve0Virtual = reserve0Virtual.sub(step.amountOut).toUint112();
                     reserve1Virtual = (uint256(reserve1Virtual) + step.amountIn).toUint112();
                 }
+
+                // TODO remove this eventually, it's simply meant to ensure our overshoot logic is correct
+                {
+                FixedPoint.uq112x112 memory priceNext = FixedPoint.fraction(reserve1Virtual, reserve0Virtual);
+                if (params.zeroForOne) {
+                    assert(priceNext._x >= step.nextPrice._x);
+                } else {
+                    assert(priceNext._x <= step.nextPrice._x);                    
+                }
+                }
+
                 amountInRemaining = amountInRemaining.sub(step.amountIn).toUint112();
                 amountOut = (uint256(amountOut) + step.amountOut).toUint112();
             }
@@ -638,6 +649,24 @@ contract UniswapV3Pair is IUniswapV3Pair {
                         reserve1Virtual = reserve1Virtual.addi(token1VirtualDelta).toUint112();
                     }
 
+                    // TODO remove this eventually, it's simply meant to show the direction of rounding
+                    {
+                    FixedPoint.uq112x112 memory priceNext = FixedPoint.fraction(reserve1Virtual, reserve0Virtual);
+                    if (params.zeroForOne) {
+                        if (token1VirtualDelta > 0) {
+                            assert(priceNext._x <= step.nextPrice._x); // this should be ok, we're moving left
+                        } else {
+                            assert(priceNext._x >= step.nextPrice._x); // this is less good, we're moving right
+                        }
+                    } else {
+                        if (token1VirtualDelta > 0) {
+                            assert(priceNext._x >= step.nextPrice._x); // this should be ok, we're moving right
+                        } else {
+                            assert(priceNext._x <= step.nextPrice._x); // this is less good, we're moving left
+                        }
+                    }
+                    }
+
                     // update virtual supply
                     // TODO it may be possible to squeeze out a bit more precision under certain circumstances by:
                     // a) summing total negative and positive token1VirtualDeltas
@@ -661,7 +690,7 @@ contract UniswapV3Pair is IUniswapV3Pair {
                     tickInfo.secondsOutside = _blockTimestamp() - tickInfo.secondsOutside; // overflow is desired
                 }
 
-                tick += params.zeroForOne ? int16(-1) : int16(1);
+                tick += params.zeroForOne ? -1 : int16(1);
             }
         }
 
