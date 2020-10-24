5af57cdb2b94828a014543e22dac82b224a82ce1
Moody Salem <moody.salem@gmail.com>
2020-10-24 13:52:10 -0500

    it's not: Revert "what if you just add 1 (is it a truncation error?)"
    This reverts commit 59155110

--- contracts/test/PriceMathEchidnaTest.sol
+++ contracts/test/PriceMathEchidnaTest.sol
@@ -17,8 +17,7 @@ contract PriceMathEchidnaTest {
     ) external pure {
         require(reserveIn > 1001 && reserveOut > 1001 && lpFee < PriceMath.LP_FEE_BASE);
 
-        uint112 amountIn = PriceMath.getInputToRatio(reserveIn, reserveOut, lpFee, FixedPoint.uq112x112(inOutRatio)) +
-            1;
+        uint112 amountIn = PriceMath.getInputToRatio(reserveIn, reserveOut, lpFee, FixedPoint.uq112x112(inOutRatio));
         uint256 amountInLessFee = (uint256(amountIn).mul(PriceMath.LP_FEE_BASE - lpFee)).div(PriceMath.LP_FEE_BASE);
 
         if (amountInLessFee == 0) {
