c26e60b9f2e4b55b25e9e1a38030e213990896e9
Moody Salem <moodysalem@users.noreply.github.com>
2020-10-28 11:22:27 -0500

    Fix pair echidna test (#109)
    * add some tests to try
    
    * make create pair private, limit swap size
    
    * add setPosition
    
    * fix the tokenA/tokenB usage
    
    * woops

--- contracts/test/UniswapV3PairEchidnaTest.sol
+++ contracts/test/UniswapV3PairEchidnaTest.sol
@@ -14,53 +14,59 @@ contract UniswapV3PairEchidnaTest {
     using SafeMath for uint256;
     using SafeCast for uint256;
 
-    UniswapV3Factory factory;
-
     TestERC20 token0;
     TestERC20 token1;
+
+    UniswapV3Factory factory;
     UniswapV3Pair pair;
 
     constructor() public {
         factory = new UniswapV3Factory(address(this));
-        createNewPair(0, 1e18, 2);
+        initializeTokens();
+        createNewPair();
+        token0.approve(address(pair), uint256(-1));
+        token1.approve(address(pair), uint256(-1));
+        initializePair(0, 1e18, 2);
     }
 
-    function createNewPair(
-        int16 tick,
-        uint112 amount0,
-        uint8 feeVote
-    ) private {
-        TestERC20 tokenA = new TestERC20(0);
-        TestERC20 tokenB = new TestERC20(0);
+    function initializeTokens() private {
+        TestERC20 tokenA = new TestERC20(uint256(-1));
+        TestERC20 tokenB = new TestERC20(uint256(-1));
         (token0, token1) = (address(tokenA) < address(tokenB) ? (tokenA, tokenB) : (tokenB, tokenA));
-        pair = UniswapV3Pair(factory.createPair(address(tokenA), address(tokenB)));
-        initialize(tick, amount0, feeVote);
     }
 
-    function initialize(
+    function createNewPair() private {
+        pair = UniswapV3Pair(factory.createPair(address(token0), address(token1)));
+    }
+
+    function initializePair(
         int16 tick,
         uint112 amount0,
         uint8 feeVote
     ) private {
-        require(tick < TickMath.MAX_TICK && tick > TickMath.MIN_TICK);
-
         FixedPoint.uq112x112 memory price = TickMath.getRatioAtTick(tick);
         uint112 amount1 = FullMath.mulDiv(amount0, price._x, uint256(1) << 112).toUint112();
-
-        token0.mint(address(this), amount0);
-        token1.mint(address(this), amount1);
-
-        token0.approve(address(pair), amount0);
-        token1.approve(address(pair), amount1);
-
         pair.initialize(amount0, amount1, tick, feeVote % pair.NUM_FEE_OPTIONS());
     }
 
-    //    function swap0For1(uint112 amount0In) external {
-    //        token0.mint(address(this), amount0In);
-    //        token0.approve(address(pair), amount0In);
-    //        pair.swap0For1(amount0In, address(this), '');
-    //    }
+    function swap0For1(uint112 amount0In) external {
+        require(amount0In < 1e18);
+        pair.swap0For1(amount0In, address(this), '');
+    }
+
+    function swap1For0(uint112 amount1In) external {
+        require(amount1In < 1e18);
+        pair.swap1For0(amount1In, address(this), '');
+    }
+
+    function setPosition(
+        int16 tickLower,
+        int16 tickUpper,
+        uint8 feeVote,
+        int112 liquidityDelta
+    ) external {
+        pair.setPosition(tickLower, tickUpper, feeVote % pair.NUM_FEE_OPTIONS(), liquidityDelta);
+    }
 
     function echidna_isInitialized() external view returns (bool) {
         return (address(token0) != address(0) &&
