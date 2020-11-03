c20f1cdf3b53a0e27b9b1f37d1195d6360bc9917
Moody Salem <moody.salem@gmail.com>
2020-11-03 10:10:28 -0600

    diff <= 1

--- contracts/test/TickMathEchidnaTest.sol
+++ contracts/test/TickMathEchidnaTest.sol
@@ -11,6 +11,10 @@ contract TickMathEchidnaTest {
     function getRatioAtTickInvariant(int16 tick) external pure {
         require(tick >= TickMath.MIN_TICK && tick < TickMath.MAX_TICK);
 
-        assert(TickMath.getRatioAtTick(tick)._x == TickMath.getRatioAtTick(-tick).reciprocal()._x);
+        uint224 ratio = TickMath.getRatioAtTick(tick)._x;
+        uint224 reciprocalNegative = TickMath.getRatioAtTick(-tick).reciprocal()._x;
+        uint224 diff = ratio > reciprocalNegative ? ratio - reciprocalNegative : reciprocalNegative - ratio;
+
+        assert(diff <= 1);
     }
 }
