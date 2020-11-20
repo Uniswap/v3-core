3b7b47b9c6a72e4cd1ae455225fa60ac772ff673
Moody Salem <moody.salem@gmail.com>
2020-11-20 15:33:29 -0600

    fix couple off by one errors

--- test/UniswapV3Pair.spec.ts
+++ test/UniswapV3Pair.spec.ts
@@ -610,7 +610,7 @@ describe('UniswapV3Pair', () => {
       await token0.approve(pair.address, constants.MaxUint256)
       await expect(pair.swap0For1(amount0In, walletAddress, '0x'))
         .to.emit(token1, 'Transfer')
-        .withArgs(pair.address, walletAddress, '95298218973436072')
+        .withArgs(pair.address, walletAddress, '95298218973436071')
 
       const tickCurrent = await pair.tickCurrent()
       expect(tickCurrent).to.eq(-10)
@@ -660,7 +660,7 @@ describe('UniswapV3Pair', () => {
       await token1.approve(pair.address, constants.MaxUint256)
       await expect(pair.swap1For0(amount1In, walletAddress, '0x'))
         .to.emit(token0, 'Transfer')
-        .withArgs(pair.address, walletAddress, '95298218973436072')
+        .withArgs(pair.address, walletAddress, '95298218973436071')
 
       const tickCurrent = await pair.tickCurrent()
       expect(tickCurrent).to.eq(9)
