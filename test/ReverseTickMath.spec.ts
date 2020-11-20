041a4ea4dbf44556b9477216adcea2f2cbbc5749
Moody Salem <moody.salem@gmail.com>
2020-11-20 11:17:46 -0600

    few more tests

--- test/ReverseTickMath.spec.ts
+++ test/ReverseTickMath.spec.ts
@@ -25,7 +25,16 @@ describe('ReverseTickMath', () => {
     it('lowerBound and upper bound are both off', async () => {
       expect(await reverseTickMath.getTickFromPrice({_x: encodePrice(1, 1)}, -3, 4)).to.eq(0)
     })
-    it('lowerBound == upperBound', async () => {
+    it('lowerBound and upper bound off by 128', async () => {
+      expect(await reverseTickMath.getTickFromPrice({_x: encodePrice(1, 1)}, -128, 128)).to.eq(0)
+    })
+    it('price is below lower bound', async () => {
+      expect(await reverseTickMath.getTickFromPrice({_x: encodePrice(1, 1)}, 5, 250)).to.eq(5)
+    })
+    it('price is above upper bound', async () => {
+      expect(await reverseTickMath.getTickFromPrice({_x: encodePrice(1, 1)}, -100, -25)).to.eq(-25)
+    })
+    it('throws if lowerBound == upperBound', async () => {
       await expect(reverseTickMath.getTickFromPrice({_x: encodePrice(1, 1)}, 0, 0)).to.be.revertedWith(
         'ReverseTickMath::getTickFromPrice: lower bound must be less than upper bound'
       )
