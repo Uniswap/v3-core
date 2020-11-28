41e1a27b9a9404c7ffb1bdc8a6a67c7c77b458a4
Connor Martin <cnr.mrtn@gmail.com>
2020-11-28 18:04:54 -0500

    updated factory

--- contracts/UniswapV3Factory.sol
+++ contracts/UniswapV3Factory.sol
@@ -5,6 +5,8 @@ import './interfaces/IUniswapV3Factory.sol';
 
 import './UniswapV3Pair.sol';
 
+/// @title The Uniswap V3 Factory contract.
+/// @dev Creates new trading pairs at deterministic addresses.
 contract UniswapV3Factory is IUniswapV3Factory {
     address public override owner;
 
@@ -14,14 +16,20 @@ contract UniswapV3Factory is IUniswapV3Factory {
     mapping(address => mapping(address => mapping(uint24 => address))) public override getPair;
     address[] public override allPairs;
 
+    /// @notice Gets length of allPairs array.
+    /// @return length of allPairs address array.
     function allPairsLength() external view override returns (uint256) {
         return allPairs.length;
     }
 
+    /// @notice Gets length of allEnabledFeeOptions array.
+    /// @return Length of allEnabledFeeOptions array.
     function allEnabledFeeOptionsLength() external view override returns (uint256) {
         return allEnabledFeeOptions.length;
     }
 
+    /// @notice The Factory contract constructor.
+    /// @param _owner The owner of the Factory contract.
     constructor(address _owner) public {
         owner = _owner;
         emit OwnerChanged(address(0), _owner);
@@ -34,6 +42,11 @@ contract UniswapV3Factory is IUniswapV3Factory {
         _enableFeeOption(24000);
     }
 
+    /// @notice Deploys a new trading pair.
+    /// @param tokenA the first token of the desired pair.
+    /// @param tokenB the second token of the desired pair.
+    /// @param fee the desired fee.
+    /// @return The address of the newly deployed pair.
     function createPair(
         address tokenA,
         address tokenB,
@@ -53,12 +66,14 @@ contract UniswapV3Factory is IUniswapV3Factory {
         emit PairCreated(token0, token1, fee, pair, allPairs.length);
     }
 
+    /// @notice Sets Factory contract owner to a new address.
     function setOwner(address _owner) external override {
         require(msg.sender == owner, 'UniswapV3Factory::setOwner: must be called by owner');
         emit OwnerChanged(owner, _owner);
         owner = _owner;
     }
 
+    /// @notice see enableFeeOption.
     function _enableFeeOption(uint24 fee) private {
         require(fee < 1000000, 'UniswapV3Factory::enableFeeOption: fee cannot be greater than or equal to 100%');
         require(isFeeOptionEnabled[fee] == false, 'UniswapV3Factory::enableFeeOption: fee option is already enabled');
@@ -68,6 +83,8 @@ contract UniswapV3Factory is IUniswapV3Factory {
         emit FeeOptionEnabled(fee);
     }
 
+    /// @notice If chosen, enables the fee option when a pair is deployed.
+    /// @param fee The chosen fee option - passed via createPair.
     function enableFeeOption(uint24 fee) external override {
         require(msg.sender == owner, 'UniswapV3Factory::enableFeeOption: must be called by owner');
 
--- contracts/interfaces/IUniswapV3Pair.sol
+++ contracts/interfaces/IUniswapV3Pair.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.5.0;
 pragma experimental ABIEncoderV2;
 
+/// @title the Uniswap V3 Pair Interface.
 interface IUniswapV3Pair {
     event Initialized(int24 tick);
 
