e7a7bc79495c236579a310f9842a193cf464287e
Moody Salem <moody.salem@gmail.com>
2020-12-07 15:07:15 -0600

    remove .only

--- test/SqrtPriceMath.spec.ts
+++ test/SqrtPriceMath.spec.ts
@@ -6,7 +6,7 @@ import {expect} from './shared/expect'
 import snapshotGasCost from './shared/snapshotGasCost'
 import {encodePriceSqrt, expandTo18Decimals} from './shared/utilities'
 
-describe.only('SqrtPriceMath', () => {
+describe('SqrtPriceMath', () => {
   let sqrtPriceMath: SqrtPriceMathTest
   before(async () => {
     const sqrtPriceMathTestFactory = await ethers.getContractFactory('SqrtPriceMathTest')
