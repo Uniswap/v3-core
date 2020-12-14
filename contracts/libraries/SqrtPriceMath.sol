42e23eda08a1280bb4e13e141ed1d335992ff273
Moody Salem <moody.salem@gmail.com>
2020-12-14 14:05:28 -0600

    add a comment

--- contracts/libraries/SqrtPriceMath.sol
+++ contracts/libraries/SqrtPriceMath.sol
@@ -16,6 +16,7 @@ library SqrtPriceMath {
     using SafeCast for uint256;
 
     function divRoundingUp(uint256 x, uint256 d) internal pure returns (uint256) {
+        // addition is safe because (uint256(-1) / 1) + (uint256(-1) % 1 > 0 ? 1 : 0) == uint256(-1)
         return (x / d) + (x % d > 0 ? 1 : 0);
     }
 
