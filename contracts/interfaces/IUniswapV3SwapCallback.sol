c9c6e635185a781d30ea7197457bc75c25ea14c7
Moody Salem <moody.salem@gmail.com>
2020-12-22 11:19:06 -0600

    add data parameter to initialize/mint/swap callbacks

--- contracts/interfaces/IUniswapV3SwapCallback.sol
+++ contracts/interfaces/IUniswapV3SwapCallback.sol
@@ -3,5 +3,9 @@ pragma solidity >=0.5.0;
 
 interface IUniswapV3SwapCallback {
     // callback sent to the caller of a swap method
-    function uniswapV3SwapCallback(int256 amount0Delta, int256 amount1Delta) external;
+    function uniswapV3SwapCallback(
+        int256 amount0Delta,
+        int256 amount1Delta,
+        bytes calldata data
+    ) external;
 }
