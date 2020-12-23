ad69ead34ace6372d992fb253d764472749d327c
Moody Salem <moody.salem@gmail.com>
2020-12-23 12:43:45 -0600

    make the addresses computable onchain!

---
+++ contracts/test/MockTimeUniswapV3PairDeployer.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+import './MockTimeUniswapV3Pair.sol';
+import '../interfaces/IUniswapV3PairDeployer.sol';
+
+contract MockTimeUniswapV3PairDeployer is IUniswapV3PairDeployer {
+    struct Parameters {
+        address factory;
+        address token0;
+        address token1;
+        uint24 fee;
+        int24 tickSpacing;
+    }
+
+    Parameters public override parameters;
+
+    event PairCreated(address pair);
+
+    function deploy(
+        address factory,
+        address token0,
+        address token1,
+        uint24 fee,
+        int24 tickSpacing
+    ) external override returns (address pair) {
+        parameters = Parameters({factory: factory, token0: token0, token1: token1, fee: fee, tickSpacing: tickSpacing});
+        pair = address(
+            new MockTimeUniswapV3Pair{salt: keccak256(abi.encodePacked(token0, token1, fee, tickSpacing))}()
+        );
+        emit PairCreated(pair);
+        delete parameters;
+    }
+}
