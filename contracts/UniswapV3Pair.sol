0ff2b0c35441fc9274b00040eee22b083e18215e
Noah Zinsmeister <noahwz@gmail.com>
2020-12-31 14:44:59 -0500

    try out typed ticks (#245)
    * try out typed ticks
    
    * Fix code style issues with Prettier
    
    * switch order of tick bitmap
    
    * Fix code style issues with Prettier
    
    * save gas with a negation
    
    * fix gas test
    
    * Revert "fix gas test"
    
    This reverts commit f3f9a8ed6750753561f6e4126c1239640bec9a4e.
    
    * Revert "save gas with a negation"
    
    This reverts commit a462e19fe9f23dccb0c64c931e5dd6d374a0b605.
    
    * Revert "switch order of tick bitmap"
    
    This reverts commit 7c169f10266ff0b1e1059e44ad1f3063c2efa973.
    
    * remove structs, clean up SpacedTickBitmap
    
    prevent poking of non-existent positions
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- contracts/UniswapV3Pair.sol
+++ contracts/UniswapV3Pair.sol
@@ -226,13 +226,13 @@ contract UniswapV3Pair is IUniswapV3Pair {
         require(tickLower < tickUpper, 'TLU');
         require(tickLower >= MIN_TICK, 'TLM');
         require(tickUpper <= MAX_TICK, 'TUM');
-        require(tickLower % tickSpacing == 0, 'TLS');
-        require(tickUpper % tickSpacing == 0, 'TUS');
 
         position = _getPosition(owner, tickLower, tickUpper);
 
         if (liquidityDelta < 0) {
             require(position.liquidity >= uint128(-liquidityDelta), 'CP');
+        } else if (liquidityDelta == 0) {
+            require(position.liquidity > 0, 'NP'); // disallow updates for 0 liquidity positions
         }
 
         Tick.Info storage tickInfoLower = _updateTick(tickLower, tick, liquidityDelta);
@@ -482,15 +482,6 @@ contract UniswapV3Pair is IUniswapV3Pair {
         uint256 feeAmount;
     }
 
-    // returns the closest parent tick that could be initialized
-    // the parent tick is the tick s.t. the input tick is gte parent tick and lt parent tick + tickSpacing
-    function closestTick(int24 tick) private view returns (int24) {
-        int24 compressed = tick / tickSpacing;
-        // round towards negative infinity
-        if (tick < 0 && tick % tickSpacing != 0) compressed--;
-        return compressed * tickSpacing;
-    }
-
     function _swap(SwapParams memory params) private {
         bool zeroForOne = params.sqrtPriceLimit._x < params.slot0Start.sqrtPriceCurrent._x;
         bool exactInput = params.amountSpecified > 0;
@@ -515,9 +506,9 @@ contract UniswapV3Pair is IUniswapV3Pair {
             step.sqrtPriceStart = state.sqrtPrice;
 
             (step.tickNext, step.initialized) = tickBitmap.nextInitializedTickWithinOneWord(
-                closestTick(state.tick),
-                zeroForOne,
-                tickSpacing
+                state.tick,
+                tickSpacing,
+                zeroForOne
             );
 
             // get the price for the next tick
