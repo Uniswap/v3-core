e4a0e66fc96d9a1310ddc471f9310e50a445d2d5
Moody Salem <moody.salem@gmail.com>
2021-01-04 18:54:50 -0600

    some comments

--- contracts/libraries/Position.sol
+++ contracts/libraries/Position.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0;
 
+// positions represent an owner account's liquidity at a given lower/upper tick combination, and store additional state
+// for tracking fees owed to the position.
 library Position {
+    // info stored for each user's position
     struct Info {
+        // the amount of liquidity owned by this position
         uint128 liquidity;
-        // fee growth per unit of liquidity as of the last modification
+        // fee growth per unit of liquidity as of the last update to liquidity or fees owed
         uint256 feeGrowthInside0LastX128;
         uint256 feeGrowthInside1LastX128;
         // the fees owed to the position owner in token0/token1
