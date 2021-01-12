498c935465b7c8138d1c20f3633a29c1f1350912
Moody Salem <moody.salem@gmail.com>
2021-01-12 10:00:18 -0600

    add a fuzz test for fee growth overflow

---
+++ contracts/test/TickOverflowSafetyEchidnaTest.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.7.6;
+
+import '../libraries/Tick.sol';
+
+contract TickOverflowSafetyEchidnaTest {
+    using Tick for mapping(int24 => Tick.Info);
+
+    int24 private constant MIN_TICK = -16;
+    int24 private constant MAX_TICK = 16;
+    uint128 private constant MAX_LIQUIDITY = uint128(-1) / 32;
+
+    mapping(int24 => Tick.Info) private ticks;
+    int24 private tick = 0;
+    // half the cap of fee growth has happened, this can overflow
+    uint256 private feeGrowthGlobal0X128 = uint256(-1) / 2;
+    uint256 private feeGrowthGlobal1X128 = uint256(-1) / 2;
+    // how much total growth has happened, this cannot overflow
+    uint256 private totalGrowth0 = 0;
+    uint256 private totalGrowth1 = 0;
+
+    function increaseFeeGrowthGlobal0X128(uint256 amount) external {
+        require(totalGrowth0 + amount > totalGrowth0); // overflow check
+        feeGrowthGlobal0X128 += amount; // overflow desired
+        totalGrowth0 += amount;
+    }
+
+    function increaseFeeGrowthGlobal1X128(uint256 amount) external {
+        require(totalGrowth1 + amount > totalGrowth1); // overflow check
+        feeGrowthGlobal1X128 += amount; // overflow desired
+        totalGrowth1 += amount;
+    }
+
+    function setPosition(
+        int24 tickLower,
+        int24 tickUpper,
+        int128 liquidityDelta
+    ) external {
+        require(tickLower > MIN_TICK);
+        require(tickUpper < MAX_TICK);
+        require(tickLower < tickUpper);
+        ticks.update(
+            tickLower,
+            tick,
+            liquidityDelta,
+            feeGrowthGlobal0X128,
+            feeGrowthGlobal1X128,
+            uint32(block.timestamp),
+            false,
+            MAX_LIQUIDITY
+        );
+        ticks.update(
+            tickUpper,
+            tick,
+            liquidityDelta,
+            feeGrowthGlobal0X128,
+            feeGrowthGlobal1X128,
+            uint32(block.timestamp),
+            true,
+            MAX_LIQUIDITY
+        );
+
+        checkTicks(tickLower, tickUpper);
+    }
+
+    function checkTicks(int24 tickLower, int24 tickUpper) public view {
+        require(tickLower > MIN_TICK);
+        require(tickUpper < MAX_TICK);
+        require(tickLower < tickUpper);
+        require(ticks[tickLower].liquidityGross > 0);
+        require(ticks[tickUpper].liquidityGross > 0);
+
+        (uint256 fgi0, uint256 fgi1) =
+            ticks.getFeeGrowthInside(tickLower, tickUpper, tick, feeGrowthGlobal0X128, feeGrowthGlobal1X128);
+        assert(fgi0 <= feeGrowthGlobal0X128);
+        assert(fgi1 <= feeGrowthGlobal1X128);
+    }
+
+    function moveToTick(int24 target) external {
+        require(target > MIN_TICK);
+        require(target < MAX_TICK);
+        while (tick != target) {
+            if (tick < target) {
+                if (ticks[tick + 1].liquidityGross > 0)
+                    ticks.cross(tick + 1, feeGrowthGlobal0X128, feeGrowthGlobal1X128, uint32(block.timestamp));
+                tick++;
+            } else {
+                if (ticks[tick].liquidityGross > 0)
+                    ticks.cross(tick, feeGrowthGlobal0X128, feeGrowthGlobal1X128, uint32(block.timestamp));
+                tick--;
+            }
+        }
+    }
+}
