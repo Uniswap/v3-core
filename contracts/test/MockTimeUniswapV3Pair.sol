af6ae9b4d79db2027ade3cdbb66f831d2cda3cdb
Noah Zinsmeister <noahwz@gmail.com>
2021-01-13 15:23:03 -0600

    add failing overflow test case

--- contracts/test/MockTimeUniswapV3Pair.sol
+++ contracts/test/MockTimeUniswapV3Pair.sol
@@ -17,4 +17,12 @@ contract MockTimeUniswapV3Pair is UniswapV3Pair {
     function _blockTimestamp() internal view override returns (uint32) {
         return time;
     }
+
+    function setFeeGrowthGlobal0X128(uint256 _feeGrowthGlobal0X128) external {
+        feeGrowthGlobal0X128 = _feeGrowthGlobal0X128;
+    }
+
+    function setFeeGrowthGlobal1X128(uint256 _feeGrowthGlobal1X128) external {
+        feeGrowthGlobal1X128 = _feeGrowthGlobal1X128;
+    }
 }
