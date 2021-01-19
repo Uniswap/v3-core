3865cee2900bc0a2636520431f1b270a19e0324d
Moody Salem <moody.salem@gmail.com>
2021-01-19 17:07:46 -0600

    start an echidna test

---
+++ contracts/test/OracleEchidnaTest.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+import './OracleTest.sol';
+
+contract OracleEchidnaTest {
+    OracleTest private oracle;
+
+    uint32 timePassed;
+
+    constructor() {
+        oracle = new OracleTest();
+    }
+
+    function initialize(
+        uint32 time,
+        int24 tick,
+        uint128 liquidity
+    ) external {
+        oracle.initialize(OracleTest.InitializeParams({time: time, tick: tick, liquidity: liquidity}));
+    }
+
+    function limitTimePassed(uint32 by) private {
+        require(timePassed + by >= timePassed);
+        timePassed += by;
+    }
+
+    function advanceTime(uint32 by) public {
+        limitTimePassed(by);
+        oracle.advanceTime(by);
+    }
+
+    // write an observation, then change tick and liquidity
+    function update(
+        uint32 advanceTimeBy,
+        int24 tick,
+        uint128 liquidity
+    ) external {
+        limitTimePassed(advanceTimeBy);
+        oracle.update(OracleTest.UpdateParams({advanceTimeBy: advanceTimeBy, tick: tick, liquidity: liquidity}));
+    }
+
+    function grow(uint16 target) external {
+        oracle.grow(target);
+    }
+
+    function echidna_indexAlwaysLtCardinality() external view returns (bool) {
+        return oracle.index() < oracle.cardinality();
+    }
+
+    function echidna_cardinalityAlwaysLteTarget() external view returns (bool) {
+        return oracle.cardinality() <= oracle.target();
+    }
+}
