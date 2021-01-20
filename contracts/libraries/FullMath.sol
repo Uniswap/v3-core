56f6559d096bb1cdad161015978835f2ec5d6bd1
Connor Martin <cnr.mrtn@gmail.com>
2021-01-20 17:19:16 -0600

    math comments

--- contracts/libraries/FullMath.sol
+++ contracts/libraries/FullMath.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: CC-BY-4.0
 pragma solidity >=0.4.0;
 
-// taken from https://medium.com/coinmonks/math-in-solidity-part-3-percents-and-proportions-4db014e080b1
-// license is CC-BY-4.0
+/// @title FullMath
+/// @notice This library provides solutions for securely handling percents and proportions in solidity.
+/// @dev taken from https://medium.com/coinmonks/math-in-solidity-part-3-percents-and-proportions-4db014e080b1
+
 library FullMath {
+    /// @notice Multiplies two 256-bit uint's, and returns the result as a 512-bit uint split into two 256-bit parts.
+    /// @param x
+    /// @param y
+    /// @return l The lower portion of an emulated 512 bit width integer
+    /// @return h The higher portion of an emulated 512 bit width integer
     function fullMul(uint256 x, uint256 y) private pure returns (uint256 l, uint256 h) {
         uint256 mm = mulmod(x, y, uint256(-1));
         l = x * y;
@@ -11,6 +18,12 @@ library FullMath {
         if (mm < l) h -= 1;
     }
 
+    /// @dev That calculates x×y÷z, rounds the result down, and throws in case z is zero or if the result 
+    ///      does not fit into uint256.
+    /// @param x
+    /// @param y
+    /// @param d 
+    /// @return  
     function mulDiv(
         uint256 x,
         uint256 y,
