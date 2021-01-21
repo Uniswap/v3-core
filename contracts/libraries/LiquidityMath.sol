427a3b44a9d26a5dbba79499996e34f586dbd1de
Connor Martin <cnr.mrtn@gmail.com>
2021-01-21 15:02:58 -0600

    Update LiquidityMath.sol

--- contracts/libraries/LiquidityMath.sol
+++ contracts/libraries/LiquidityMath.sol
@@ -3,9 +3,7 @@ pragma solidity >=0.5.0;
 
 // contains functions for applying signed liquidity delta values to unsigned liquidity values
 library LiquidityMath {
-    /// @param x
-    /// @param y
-    /// @return z
+
     function addDelta(uint128 x, int128 y) internal pure returns (uint128 z) {
         if (y < 0) {
             require((z = x - uint128(-y)) < x, 'LS');
@@ -14,9 +12,6 @@ library LiquidityMath {
         }
     }
 
-    /// @param x
-    /// @param y
-    /// @return z
     function subDelta(uint128 x, int128 y) internal pure returns (uint128 z) {
         if (y < 0) {
             require((z = x + uint128(-y)) > x, 'LA');
