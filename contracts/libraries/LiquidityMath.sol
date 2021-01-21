375622cd613ba8b490bd65be746757793cd1dba5
Connor Martin <cnr.mrtn@gmail.com>
2021-01-21 15:00:59 -0600

    removed unfinished comments

--- contracts/libraries/LiquidityMath.sol
+++ contracts/libraries/LiquidityMath.sol
@@ -1,13 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0;
 
-/// @title LiquidityMath
-/// @notice contains functions for applying signed liquidity delta values to unsigned liquidity values
+// contains functions for applying signed liquidity delta values to unsigned liquidity values
 library LiquidityMath {
     
-    /// @param x 
-    /// @param y 
-    /// @return z
     function addDelta(uint128 x, int128 y) internal pure returns (uint128 z) {
         if (y < 0) {
             require((z = x - uint128(-y)) < x, 'LS');
@@ -16,9 +12,6 @@ library LiquidityMath {
         }
     }
 
-    /// @param x 
-    /// @param y
-    /// @return z
     function subDelta(uint128 x, int128 y) internal pure returns (uint128 z) {
         if (y < 0) {
             require((z = x + uint128(-y)) > x, 'LA');
