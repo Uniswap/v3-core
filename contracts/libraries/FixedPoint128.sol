026d5822c4bda255fa9de4f7220bf50411fd9aaa
Connor Martin <cnr.mrtn@gmail.com>
2021-01-22 09:39:06 -0600

    comments for FixedPoint96, FixedPoint128, FullMath (#314)
    * math comments
    
    * Update FullMath.sol
    
    * full math draft
    
    * Fix code style issues with Prettier
    
    * removed unfinished comments
    
    * Update LiquidityMath.sol
    
    * Fix code style issues with Prettier
    
    * clean
    
    * updated re review
    
    * Update FullMath.sol
    
    * Update contracts/libraries/FullMath.sol
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>
    Co-authored-by: Noah Zinsmeister <noahwz@gmail.com>

--- contracts/libraries/FixedPoint128.sol
+++ contracts/libraries/FixedPoint128.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.4.0;
 
-// a library for handling binary fixed point numbers (https://en.wikipedia.org/wiki/Q_(number_format))
+/// @title FixedPoint128
+/// @notice A library for handling binary fixed point numbers, see https://en.wikipedia.org/wiki/Q_(number_format)
 library FixedPoint128 {
     uint8 private constant RESOLUTION = 128;
     uint256 internal constant Q128 = 0x100000000000000000000000000000000;
