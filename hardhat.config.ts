752f4bb8c142a8bc317729f6a3b23630885f9ef9
Moody Salem <moody.salem@gmail.com>
2021-01-24 21:10:19 -0600

    strip revert reasons

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,7 +13,16 @@ export default {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 50,
+        runs: 2000,
+      },
+      debug: {
+        // How to treat revert (and require) reason strings. Settings are
+        // "default", "strip", "debug" and "verboseDebug".
+        // "default" does not inject compiler-generated revert strings and keeps user-supplied ones.
+        // "strip" removes all revert strings (if possible, i.e. if literals are used) keeping side-effects
+        // "debug" injects strings for compiler-generated internal reverts, implemented for ABI encoders V1 and V2 for now.
+        // "verboseDebug" even appends further information to user-supplied revert strings (not yet implemented)
+        revertStrings: 'strip',
       },
       metadata: {
         // do not include the metadata hash, since this is machine dependent
