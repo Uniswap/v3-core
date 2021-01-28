8f2d785f8b757746930868c4686111bef766fe9e
Moody Salem <moody.salem@gmail.com>
2021-01-27 18:27:23 -0600

    first pass of some revert reasons that are not useful

--- contracts/libraries/BitMath.sol
+++ contracts/libraries/BitMath.sol
@@ -11,7 +11,7 @@ library BitMath {
     /// @param x the value for which to compute the most significant bit, must be greater than 0
     /// @return r the index of the most significant bit
     function mostSignificantBit(uint256 x) internal pure returns (uint8 r) {
-        require(x > 0, 'MSB');
+        require(x > 0);
 
         if (x >= 0x100000000000000000000000000000000) {
             x >>= 128;
@@ -51,7 +51,7 @@ library BitMath {
     /// @param x the value for which to compute the least significant bit, must be greater than 0
     /// @return r the index of the least significant bit
     function leastSignificantBit(uint256 x) internal pure returns (uint8 r) {
-        require(x > 0, 'LSB');
+        require(x > 0);
 
         r = 255;
         if (x & type(uint128).max > 0) {
