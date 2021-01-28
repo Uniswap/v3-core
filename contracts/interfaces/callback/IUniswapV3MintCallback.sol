0d9ebd9c1db9ab521ac278e68c12b7e379f46301
Moody Salem <moody.salem@gmail.com>
2021-01-28 09:05:12 -0600

    document callbacks

--- contracts/interfaces/callback/IUniswapV3MintCallback.sol
+++ contracts/interfaces/callback/IUniswapV3MintCallback.sol
@@ -1,8 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0;
 
+/// @title Callback for IUniswapV3PairActions#mint
+/// @notice A contract that calls IUniswapV3PairActions#mint must implement this interface
 interface IUniswapV3MintCallback {
-    // callback sent to the caller of the mint method to collect payment
+    /// @notice Called on `msg.sender` after making updates to the position to allow the sender to pay the tokens
+    ///     due for the minted liquidity
+    /// @param amount0Owed the amount of token0 due to the pair for the minted liquidity
+    /// @param amount1Owed the amount of token1 due to the pair for the minted liquidity
+    /// @param data any data passed through by the caller via the IUniswapV3PairActions#mint call
+    /// @dev The caller of this method must be checked to be a UniswapV3Pair deployed by the canonical factory
+    /// TODO: user experience of the below note: https://github.com/Uniswap/uniswap-v3-core/issues/359
+    /// @dev This method will not be called if the amount0Owed and amount1Owed are both 0
     function uniswapV3MintCallback(
         uint256 amount0Owed,
         uint256 amount1Owed,
--- contracts/interfaces/callback/IUniswapV3SwapCallback.sol
+++ contracts/interfaces/callback/IUniswapV3SwapCallback.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0;
 
+/// @title Callback for IUniswapV3PairActions#swap
+/// @notice A contract that calls IUniswapV3PairActions#swap must implement this interface
 interface IUniswapV3SwapCallback {
-    // callback sent to the caller of a swap method
+    /// @notice Called on `msg.sender` after performing a swap and transferring any output tokens to the recipient
+    /// @param amount0Delta the amount of token0 that was sent (negative) or must be received (positive) by the pair by
+    ///     the end of the swap. If positive, the callback must send that amount of token0 to the pair.
+    /// @param amount1Delta the amount of token1 that was sent (negative) or must be received (positive) by the pair by
+    ///     the end of the swap. If positive, the callback must send that amount of token1 to the pair.
+    /// @param data any data passed through by the caller via the IUniswapV3PairActions#swap call
+    /// @dev The caller of this method must be checked to be a UniswapV3Pair deployed by the canonical factory
     function uniswapV3SwapCallback(
         int256 amount0Delta,
         int256 amount1Delta,
