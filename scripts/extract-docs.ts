03a4a4fe44a9b0b93a4ec9b3786869d3a9554839
Moody Salem <moody.salem@gmail.com>
2021-02-02 15:18:50 -0600

    generate all the docs

--- scripts/extract-docs.ts
+++ scripts/extract-docs.ts
@@ -14,7 +14,7 @@ const FILENAMES_DOCS_GENERATE = ['contracts/UniswapV3Pair.sol', 'contracts/Unisw
 
 const output = Object.keys(buildOutput.output.contracts).reduce((results, fileName) => {
   const contractsInFile = Object.keys(buildOutput.output.contracts[fileName])
-  if (FILENAMES_DOCS_GENERATE.indexOf(fileName) === -1 || fileName.startsWith('interfaces/')) return results
+  if (FILENAMES_DOCS_GENERATE.indexOf(fileName) === -1 && !fileName.startsWith('contracts/interfaces/')) return results
 
   return {
     ...results,
