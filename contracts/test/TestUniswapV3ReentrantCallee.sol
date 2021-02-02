711e7331a2cdce13d7694799ac1623f32fd9d277
Lint Action <lint-action@samuelmeuli.com>
2021-02-02 22:39:05 +0000

    Fix code style issues with Prettier

--- contracts/test/TestUniswapV3ReentrantCallee.sol
+++ contracts/test/TestUniswapV3ReentrantCallee.sol
@@ -11,55 +11,48 @@ contract TestUniswapV3ReentrantCallee is IUniswapV3SwapCallback {
     string private constant expectedReason = 'LOK';
 
     function swapToReenter(address pair) external {
-        IUniswapV3Pair(pair).swap(
-            address(0),
-            false,
-            1,
-            SqrtTickMath.MAX_SQRT_RATIO - 1,
-            new bytes(0)
-        );
+        IUniswapV3Pair(pair).swap(address(0), false, 1, SqrtTickMath.MAX_SQRT_RATIO - 1, new bytes(0));
     }
 
-    function uniswapV3SwapCallback(int256, int256, bytes calldata) external override {
+    function uniswapV3SwapCallback(
+        int256,
+        int256,
+        bytes calldata
+    ) external override {
         // try to reenter swap
-        try IUniswapV3Pair(msg.sender).swap(address(0), false, 1, 0, new bytes(0)) {}
-        catch Error(string memory reason) {
+        try IUniswapV3Pair(msg.sender).swap(address(0), false, 1, 0, new bytes(0)) {} catch Error(
+            string memory reason
+        ) {
             require(keccak256(abi.encode(reason)) == keccak256(abi.encode(expectedReason)));
         }
 
         // try to reenter poke
-        try IUniswapV3Pair(msg.sender).poke(address(0), 0, 0) {}
-        catch Error(string memory reason) {
+        try IUniswapV3Pair(msg.sender).poke(address(0), 0, 0) {} catch Error(string memory reason) {
             require(keccak256(abi.encode(reason)) == keccak256(abi.encode(expectedReason)));
         }
 
         // try to reenter mint
-        try IUniswapV3Pair(msg.sender).mint(address(0), 0, 0, 0, new bytes(0)) {}
-        catch Error(string memory reason) {
+        try IUniswapV3Pair(msg.sender).mint(address(0), 0, 0, 0, new bytes(0)) {} catch Error(string memory reason) {
             require(keccak256(abi.encode(reason)) == keccak256(abi.encode(expectedReason)));
         }
 
         // try to reenter collect
-        try IUniswapV3Pair(msg.sender).collect(address(0), 0, 0, 0, 0) {}
-        catch Error(string memory reason) {
+        try IUniswapV3Pair(msg.sender).collect(address(0), 0, 0, 0, 0) {} catch Error(string memory reason) {
             require(keccak256(abi.encode(reason)) == keccak256(abi.encode(expectedReason)));
         }
 
         // try to reenter burn
-        try IUniswapV3Pair(msg.sender).burn(address(0), 0, 0, 0) {}
-        catch Error(string memory reason) {
+        try IUniswapV3Pair(msg.sender).burn(address(0), 0, 0, 0) {} catch Error(string memory reason) {
             require(keccak256(abi.encode(reason)) == keccak256(abi.encode(expectedReason)));
         }
 
         // try to reenter flash
-        try IUniswapV3Pair(msg.sender).flash(address(0), 0, 0, new bytes(0)) {}
-        catch Error(string memory reason) {
+        try IUniswapV3Pair(msg.sender).flash(address(0), 0, 0, new bytes(0)) {} catch Error(string memory reason) {
             require(keccak256(abi.encode(reason)) == keccak256(abi.encode(expectedReason)));
         }
 
         // try to reenter collectProtocol
-        try IUniswapV3Pair(msg.sender).collectProtocol(address(0), 0, 0) {}
-        catch Error(string memory reason) {
+        try IUniswapV3Pair(msg.sender).collectProtocol(address(0), 0, 0) {} catch Error(string memory reason) {
             require(keccak256(abi.encode(reason)) == keccak256(abi.encode(expectedReason)));
         }
 
