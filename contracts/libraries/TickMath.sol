d200a97b0906aac20eb77005ae0f44fbb41048d7
Moody Salem <moody.salem@gmail.com>
2021-02-03 10:39:10 -0600

    add a comment, add to the echidna test

--- contracts/libraries/TickMath.sol
+++ contracts/libraries/TickMath.sol
@@ -46,6 +46,9 @@ library TickMath {
 
         if (tick > 0) ratio = type(uint256).max / ratio;
 
+        // this divides by 1<<32 rounding up to go from a Q128.128 to a Q128.96.
+        // we then downcast because we know the result always fits within 160 bits due to our tick input constraint
+        // we round up in the division so getTickAtSqrtRatio of the output price is always consistent
         sqrtPriceX96 = uint160((ratio >> 32) + (ratio % (1 << 32) == 0 ? 0 : 1));
     }
 
