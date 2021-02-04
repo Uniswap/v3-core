327ff95f31fbb164e32f2a6fb1518c3b82c60a5e
Connor Martin <cnr.mrtn@gmail.com>
2021-02-03 18:58:40 -0600

    capitalization and cleanup

--- contracts/interfaces/pair/IUniswapV3PairDerivedState.sol
+++ contracts/interfaces/pair/IUniswapV3PairDerivedState.sol
@@ -7,18 +7,18 @@ pragma solidity >=0.5.0;
 interface IUniswapV3PairDerivedState {
     /// @notice Returns a relative timestamp value representing how long in seconds the pair has spent between
     /// tickLower and tickUpper
-    /// @param tickLower the lower tick of the range for which to get the seconds inside
-    /// @param tickUpper the upper tick of the range for which to get the seconds inside
-    /// @return a relative timestamp for how long the pair spent in the tick range
+    /// @param tickLower The lower tick of the range for which to get the seconds inside
+    /// @param tickUpper The upper tick of the range for which to get the seconds inside
+    /// @return A relative timestamp for how long the pair spent in the tick range
     /// @dev This timestamp is strictly relative. To get a useful elapsed time (i.e. duration) value, the value returned
     /// by this method should be checkpointed externally after a position is minted and again before a position is
     /// burned. Thus the external contract must control the lifecycle of the position.
     function secondsInside(int24 tickLower, int24 tickUpper) external view returns (uint32);
 
     /// @notice Returns the cumulative tick and liquidity as of a timestamp secondsAgo from the current block timestamp.
-    /// @param secondsAgo how long ago the cumulative tick and liquidity should be returned from
-    /// @return tickCumulative cumulative tick value as of `secondsAgo` from the current block timestamp
-    /// @return liquidityCumulative cumulative liquidity-in-range value as of `secondsAgo` from the current block
+    /// @param secondsAgo How long ago the cumulative tick and liquidity should be returned from
+    /// @return tickCumulative Cumulative tick value as of `secondsAgo` from the current block timestamp
+    /// @return liquidityCumulative Cumulative liquidity-in-range value as of `secondsAgo` from the current block
     /// timestamp
     /// @dev To get a time weighted average tick or liquidity-in-range, you must call this twice, once with the start
     /// of the period and again with the end of the period. E.g. to get the last hour time-weighted average tick,
