a7d4c3bc4b695e5110c76da189a10bea97e578e7
Connor Martin <cnr.mrtn@gmail.com>
2021-02-04 19:12:09 -0600

    IERC20 minimal comments (#380)
    * Update IERC20Minimal.sol
    
    * Update IERC20Minimal.sol

--- contracts/interfaces/IERC20Minimal.sol
+++ contracts/interfaces/IERC20Minimal.sol
@@ -5,19 +5,34 @@ pragma solidity >=0.5.0;
 /// @notice Contains a subset of the full ERC20 interface that is used in Uniswap V3
 interface IERC20Minimal {
     /// @notice Returns the balance of a token
+    /// @param account The address of the account whose balance will be returned
+    /// @return The account balance
     function balanceOf(address account) external view returns (uint256);
 
     /// @notice Transfers the amount of token from the `msg.sender` to the recipient
+    /// @param recipient The recipient of the transfer
+    /// @param amount The amount of the transfer
+    /// @return Returns true for a successful transfer, false for an unsuccessful transfer
     function transfer(address recipient, uint256 amount) external returns (bool);
 
-    /// @notice Returns the current allowance given to a spender by an owner.
+    /// @notice Returns the current allowance given to a spender by an owner
+    /// @param owner The account of the token owner
+    /// @param spender The account of the token spender
+    /// @return The current allowance granted by `owner` to `spender`
     function allowance(address owner, address spender) external view returns (uint256);
 
     /// @notice Sets the allowance of a spender from the `msg.sender` of the amount `amount`
+    /// @param spender The account which will be allowed to spend a given amount of the owners tokens
+    /// @param amount The amount of tokens allowed to be used by `spender`
+    /// @return Returns true for a successful approval, false for unsuccessful
     function approve(address spender, uint256 amount) external returns (bool);
 
-    /// @notice Transfers from the sender, and deducts from the allowance of the sender.
-    /// The allowance of `msg.sender` must be greater than or equal to the amount.
+    /// @notice Transfers `amount` tokens from sender to recipient using the allowance mechanism. amount is then deducted from the caller’s allowance
+    /// @dev The allowance of `msg.sender` must be greater than or equal to the `amount`
+    /// @param sender The account from which the transfer will be initiated
+    /// @param recipient The recipient of the transfer
+    /// @param amount The amount of the transfer
+    /// @return Returns true for a successful transfer, false for unsuccessful
     function transferFrom(
         address sender,
         address recipient,
