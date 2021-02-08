1d19e52556b4a9f0e737f19012905ea6d84bb9a9
Connor Martin <cnr.mrtn@gmail.com>
2021-02-08 11:58:52 -0600

    Revert "exact in steps"
    This reverts commit b1e36dd7266534ddc8bacbcfcf9f3e1c7f570c58.

--- contracts/test/TestUniswapV3Router.sol
+++ contracts/test/TestUniswapV3Router.sol
@@ -194,15 +194,15 @@ contract TestUniswapV3Router is IUniswapV3SwapCallback {
         address[] memory pairs,
         uint256 amount0In
     ) external {
-        console.log('start endless Exact 0 for 1');
+        console.log('start endless 0 for exact 1');
         bool originZeroForOne = true;
         uint numRemainingSwaps = pairs.length;
         numRemainingSwaps--;
         console.log('initiating first swap');
-        IUniswapV3Pair(pairs[0]).swap( 
+        IUniswapV3Pair(pairs[numRemainingSwaps]).swap( 
             recipient,
             true,
-            amount0In.toInt256(),
+            -amount1Out.toInt256(),
             TickMath.MIN_SQRT_RATIO + 1,
             abi.encode(numRemainingSwaps, originZeroForOne, pairs, msg.sender)
         );
@@ -229,16 +229,14 @@ contract TestUniswapV3Router is IUniswapV3SwapCallback {
                 amount0Delta > 0 ? IUniswapV3Pair(msg.sender).token0() : IUniswapV3Pair(msg.sender).token1();
             int256 amountToBePaid = 
                 amount0Delta > 0 ? amount0Delta : amount1Delta;
-            bool exactIn = 
-                amountToBePaid < 0 ? true : false;
             bool zeroForOne = 
                 tokenToBePaid == IUniswapV3Pair(pairs[numRemainingSwaps]).token1();
 
 
-            IUniswapV3Pair(pairs[exactIn ? (pairs.length) - numRemainingSwaps - 1 : numRemainingSwaps]).swap(
+            IUniswapV3Pair(pairs[numRemainingSwaps]).swap(
                 msg.sender,
                 zeroForOne,
-                exactIn ? amountToBePaid : -amountToBePaid,
+                -amountToBePaid,
                 zeroForOne ? TickMath.MIN_SQRT_RATIO + 1 : TickMath.MAX_SQRT_RATIO - 1,
                 abi.encode(numRemainingSwaps, originZeroForOne, pairs, payer)
             );
