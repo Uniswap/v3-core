ea241ad85ae36d226cc008f0483b254ceffd0109
Connor Martin <cnr.mrtn@gmail.com>
2021-03-18 17:48:40 -0600

    natspec loose ends

--- contracts/libraries/UnsafeMath.sol
+++ contracts/libraries/UnsafeMath.sol
@@ -6,6 +6,9 @@ pragma solidity >=0.5.0;
 library UnsafeMath {
     /// @notice Returns ceil(x / y)
     /// @dev panics if y == 0
+    /// @param x The dividend
+    /// @param y The divisor
+    /// @return z The quotient, ceil(x / y)
     function divRoundingUp(uint256 x, uint256 y) internal pure returns (uint256 z) {
         // addition is safe because (type(uint256).max / 1) + (type(uint256).max % 1 > 0 ? 1 : 0) == type(uint256).max
         z = (x / y) + (x % y > 0 ? 1 : 0);
