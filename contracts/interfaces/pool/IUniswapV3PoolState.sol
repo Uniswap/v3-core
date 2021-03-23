319d7621bc914da4851ec38a5915305aaf25b43a
Noah Zinsmeister <noahwz@gmail.com>
2021-03-23 12:13:53 -0400

    remove extra return value from docs

--- contracts/interfaces/pool/IUniswapV3PoolState.sol
+++ contracts/interfaces/pool/IUniswapV3PoolState.sol
@@ -14,7 +14,6 @@ interface IUniswapV3PoolState {
     /// observationIndex The index of the last oracle observation that was written,
     /// observationCardinality The current maximum number of observations stored in the pool,
     /// observationCardinalityNext The next maximum number of observations, to be updated when the observation.
-    /// index The index of the last element written to the observation array.
     /// feeProtocol The protocol fee for both tokens of the pool.
     /// Encoded as two 4 bit values, where the protocol fee of token1 is shifted 4 bits and the protocol fee of token0
     /// is the lower 4 bits. Used as the denominator of a fraction of the swap fee, e.g. 4 means 1/4th of the swap fee.
