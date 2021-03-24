35e978a9ffc3e0070df4904ba5704ac0d9c9b9ec
Tony Sosa <tony.sosa@tonysosa.com>
2021-03-24 09:35:19 -0500

    Update IUniswapV3Factory.sol

--- contracts/interfaces/IUniswapV3Factory.sol
+++ contracts/interfaces/IUniswapV3Factory.sol
@@ -34,7 +34,7 @@ interface IUniswapV3Factory {
     function owner() external view returns (address);
 
     /// @notice Returns the tick spacing for a given fee amount, if enabled, or 0 if not enabled
-    /// @dev A fee amount can never bee removed, so this value should be hard coded or cached in the calling context
+    /// @dev A fee amount can never be removed, so this value should be hard coded or cached in the calling context
     /// @param fee The enabled fee, denominated in hundredths of a bip. Returns 0 in case of unenabled fee
     /// @return The tick spacing
     function feeAmountTickSpacing(uint24 fee) external view returns (int24);
