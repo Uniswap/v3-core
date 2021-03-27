68c9e2a5f91c25b19a8ef42c2ff26a4454e04c60
Moody Salem <moody.salem@gmail.com>
2021-03-27 10:32:47 -0500

    cheaper assembly version

--- contracts/libraries/UnsafeMath.sol
+++ contracts/libraries/UnsafeMath.sol
@@ -5,11 +5,13 @@ pragma solidity >=0.5.0;
 /// @notice Contains methods that perform common math functions but do not do any overflow or underflow checks
 library UnsafeMath {
     /// @notice Returns ceil(x / y)
-    /// @dev panics if y == 0, unless x == 0 in which case it returns 0
+    /// @dev division by 0 has unspecified behavior, and must be checked externally
     /// @param x The dividend
     /// @param y The divisor
     /// @return z The quotient, ceil(x / y)
     function divRoundingUp(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        z = x > 0 ? (x - 1) / y + 1 : 0;
+        assembly {
+            z := add(div(x, y), gt(mod(x, y), 0))
+        }
     }
 }
