fa426cf610a6375b57b2c3709dab8697ee42d811
Georgios Konstantopoulos <me@gakonst.com>
2021-05-20 15:35:54 +0300

    test: use Optimism 0.4.0

--- scripts/run-optimism.sh
+++ scripts/run-optimism.sh
@@ -10,6 +10,8 @@ current_dir=$(dirname "$0")
 
 git clone https://github.com/ethereum-optimism/optimism.git
 cd optimism/ops
+# TODO: Remove once the v0.4.0 regenesis is merged.
+git checkout regenesis/0.4.0
 
 # enabling the Docker BuildKit is recommended
 export COMPOSE_DOCKER_CLI_BUILD=1
