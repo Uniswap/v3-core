05438e15604589876b61bc32b2d141ed6565cbf9
LeadDev <leaddev@anon.farm>
2021-12-10 16:19:56 +0530

    add compability for solc 0.8

--- contracts/libraries/FullMath.sol
+++ contracts/libraries/FullMath.sol
@@ -61,7 +61,8 @@ library FullMath {
         // Factor powers of two out of denominator
         // Compute largest power of two divisor of denominator.
         // Always >= 1.
-        uint256 twos = -denominator & denominator;
+        // Cannot overflow because denominator != 0
+        uint256 twos = (type(uint256).max - denominator + 1) & denominator;
         // Divide denominator by power of two
         assembly {
             denominator := div(denominator, twos)
@@ -76,24 +77,30 @@ library FullMath {
         // If twos is zero, then it becomes one
         assembly {
             twos := add(div(sub(0, twos), twos), 1)
+            prod0 := or(prod0, mul(prod1, twos))
         }
-        prod0 |= prod1 * twos;
 
         // Invert denominator mod 2**256
         // Now that denominator is an odd number, it has an inverse
         // modulo 2**256 such that denominator * inv = 1 mod 2**256.
         // Compute the inverse by starting with a seed that is correct
         // correct for four bits. That is, denominator * inv = 1 mod 2**4
-        uint256 inv = (3 * denominator) ^ 2;
+        uint256 inv;
+
+        assembly {
+            inv := xor(mul(3, denominator), 2)
+        }
         // Now use Newton-Raphson iteration to improve the precision.
         // Thanks to Hensel's lifting lemma, this also works in modular
         // arithmetic, doubling the correct bits in each step.
-        inv *= 2 - denominator * inv; // inverse mod 2**8
-        inv *= 2 - denominator * inv; // inverse mod 2**16
-        inv *= 2 - denominator * inv; // inverse mod 2**32
-        inv *= 2 - denominator * inv; // inverse mod 2**64
-        inv *= 2 - denominator * inv; // inverse mod 2**128
-        inv *= 2 - denominator * inv; // inverse mod 2**256
+        assembly {
+            inv := mul(inv, sub(2, mul(denominator, inv))) // inverse mod 2**8
+            inv := mul(inv, sub(2, mul(denominator, inv))) // inverse mod 2**16
+            inv := mul(inv, sub(2, mul(denominator, inv))) // inverse mod 2**32
+            inv := mul(inv, sub(2, mul(denominator, inv))) // inverse mod 2**64
+            inv := mul(inv, sub(2, mul(denominator, inv))) // inverse mod 2**128
+            inv := mul(inv, sub(2, mul(denominator, inv))) // inverse mod 2**256
+        }
 
         // Because the division is now exact we can divide by multiplying
         // with the modular inverse of denominator. This will give us the
@@ -101,7 +108,9 @@ library FullMath {
         // that the outcome is less than 2**256, this is the final result.
         // We don't need to compute the high bits of the result and prod1
         // is no longer required.
-        result = prod0 * inv;
+        assembly {
+            result := mul(prod0, inv)
+        }
         return result;
     }
 
