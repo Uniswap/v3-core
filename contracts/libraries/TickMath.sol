7fedb1c85382edb40cf86661a89e34d5252a06d2
Wilson Cusack <cusackw@gmail.com>
2022-01-17 15:55:36 -0500

    constrain TickMath <0.8.0

--- contracts/libraries/TickMath.sol
+++ contracts/libraries/TickMath.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity >=0.5.0;
+pragma solidity >=0.5.0 <0.8.0;
 
 /// @title Math library for computing sqrt prices from ticks and vice versa
 /// @notice Computes sqrt price for ticks of size 1.0001, i.e. sqrt(1.0001^tick) as fixed point Q64.96 numbers. Supports
