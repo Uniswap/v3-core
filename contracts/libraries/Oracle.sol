670f009f2bf8097c43b545389fee0caacaa7d96c
Noah Zinsmeister <noahwz@gmail.com>
2022-01-26 15:41:20 -0500

    add a few more pragma restrictions

--- contracts/libraries/Oracle.sol
+++ contracts/libraries/Oracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity >=0.5.0;
+pragma solidity >=0.5.0 <0.8.0;
 
 /// @title Oracle
 /// @notice Provides price and liquidity data useful for a wide variety of system designs
