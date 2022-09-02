f78765ec85ad0f86279925027d85b48240516ab0
Swapnil Raj <raj.swapnil99@gmail.com>
2022-09-02 16:29:50 +0100

    Fix TickMath tests

--- test/TickMath.starknet.ts
+++ test/TickMath.starknet.ts
@@ -11,6 +11,8 @@ const MAX_TICK = 887272
 export const MIN_SQRT_RATIO = BigNumber.from('4295128739')
 export const MAX_SQRT_RATIO = BigNumber.from('1461446703485210103287273052203988822378723970342')
 
+bn.config({ EXPONENTIAL_AT: 999999, DECIMAL_PLACES: 40 })
+
 // returns the sqrt price as a 64x96
 export function encodePriceSqrt(reserve1: BigNumberish, reserve0: BigNumberish): BigNumber {
   return BigNumber.from(
