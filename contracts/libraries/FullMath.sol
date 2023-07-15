e472a9ef5942b22e2c75af7f6f26c1496476cc7d
Swapnapratim <swapnapratim4534@gmail.com>
2023-07-15 12:13:41 +0530

    FIXED ISSUE #641

--- contracts/libraries/FullMath.sol
+++ contracts/libraries/FullMath.sol
@@ -61,7 +61,7 @@ library FullMath {
         // Factor powers of two out of denominator
         // Compute largest power of two divisor of denominator.
         // Always >= 1.
-        uint256 twos = -denominator & denominator;
+        uint256 twos = denominator & (~denominator + 1); // unary operator cannot be used with type uint
         // Divide denominator by power of two
         assembly {
             denominator := div(denominator, twos)
