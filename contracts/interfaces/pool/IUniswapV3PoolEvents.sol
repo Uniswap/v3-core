2a9d231567ea038749a95274964599cc09bf7315
ChuckGao <chuckgao0223@gmail.com>
2023-07-23 22:53:45 +0800

    fix: update "amount0" -> "amount1" for CollectProtocol event of pool

--- contracts/interfaces/pool/IUniswapV3PoolEvents.sol
+++ contracts/interfaces/pool/IUniswapV3PoolEvents.sol
@@ -116,6 +116,6 @@ interface IUniswapV3PoolEvents {
     /// @param sender The address that collects the protocol fees
     /// @param recipient The address that receives the collected protocol fees
     /// @param amount0 The amount of token0 protocol fees that is withdrawn
-    /// @param amount0 The amount of token1 protocol fees that is withdrawn
+    /// @param amount1 The amount of token1 protocol fees that is withdrawn
     event CollectProtocol(address indexed sender, address indexed recipient, uint128 amount0, uint128 amount1);
 }
