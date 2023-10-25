fb467e67ab8aaec0e286aae5c6089b8a03ff484e
partychad <partychad@protonmail.com>
2023-10-25 13:21:00 +0200

    Perf: little gas optimization and some comments

--- contracts/UniswapV3Pool.sol
+++ contracts/UniswapV3Pool.sol
@@ -811,10 +811,11 @@ contract UniswapV3Pool is IUniswapV3Pool, NoDelegateCall {
                     slot0Start.observationCardinalityNext
                 );
             // Previous Tick (slot0Start.tick) is different from the active tick (state.tick) so we must close all the associated positions
-            address[] memory memActivePositions = activePositions[uint24(slot0Start.tick)];
+            address[] memory memActivePositions = activePositions[uint24(slot0Start.tick)]; // address[] storage positionsToClose = activePositions[uint24(slot0Start.tick)] this can even be cheaper after EIP2929, need to test with foundry
             uint numberOfPositionsToCLose = memActivePositions.length;
-            for (uint i = 0; i < numberOfPositionsToCLose; ++i) {
+            for (uint i = 0; i < numberOfPositionsToCLose;) { // if the list is too long, there is a chance we might run out of gas
                 collectLimitOrder(memActivePositions[i], slot0Start.tick);
+                unchecked { ++i; } // pre increment and unchecked scope for gas efficiency
             }
             delete activePositions[uint24(slot0Start.tick)];
 
