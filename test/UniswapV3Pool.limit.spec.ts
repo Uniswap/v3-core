2b38c21aa1b7b18bac111945678591eabd54e897
yuriarthf <yuriarthf@protonmail.com>
2023-11-03 13:23:31 +0700

    feat: test for 2 limit order

--- test/UniswapV3Pool.limit.spec.ts
+++ test/UniswapV3Pool.limit.spec.ts
@@ -31,7 +31,8 @@ describe('UniswapV3Pool limit orders tests', () => {
     const INIT_LIQUIDITY = encodePriceSqrt(1, 1)
 
     let wallet: Wallet;
-    let lpRecipient: Wallet;
+    let lpRecipient1: Wallet;
+    let lpRecipient2: Wallet;
     let pool: MockTimeUniswapV3Pool;
     let token0: TestERC20
     let token1: TestERC20
@@ -39,7 +40,7 @@ describe('UniswapV3Pool limit orders tests', () => {
 
     beforeEach('deploy pool', async () => {
         // Create wallets
-        [wallet, lpRecipient] = await (ethers as any).getSigners()
+        [wallet, lpRecipient1, lpRecipient2] = await (ethers as any).getSigners()
 
         const {
             token0: _token0,
@@ -68,15 +69,15 @@ describe('UniswapV3Pool limit orders tests', () => {
 
     it('Create limit order to tick greater than current tick', async () => {
         // Should emit 'Mint' event
-        let amount = expandTo18Decimals(5);
+        let amount = expandTo18Decimals(5)
         let tickLower = (await pool.slot0()).tick + await pool.tickSpacing()
         await expect(pool.createLimitOrder(
-            await lpRecipient.getAddress(),
+            await lpRecipient1.getAddress(),
             tickLower,
             amount
         )).to.emit(pool, 'Mint').withArgs(
             await wallet.getAddress(),
-            await lpRecipient.getAddress(),
+            await lpRecipient1.getAddress(),
             tickLower,
             tickLower + await pool.tickSpacing(),
             amount,
@@ -87,15 +88,15 @@ describe('UniswapV3Pool limit orders tests', () => {
 
     it('Create limit order to tick smaller than current tick', async () => {
         // Should emit 'Mint' event
-        let amount = expandTo18Decimals(5);
+        let amount = expandTo18Decimals(5)
         let tickLower = (await pool.slot0()).tick - await pool.tickSpacing()
         await expect(pool.createLimitOrder(
-            await lpRecipient.getAddress(),
+            await lpRecipient1.getAddress(),
             tickLower,
             amount
         )).to.emit(pool, 'Mint').withArgs(
             await wallet.getAddress(),
-            await lpRecipient.getAddress(),
+            await lpRecipient1.getAddress(),
             tickLower,
             tickLower + await pool.tickSpacing(),
             amount,
@@ -106,10 +107,10 @@ describe('UniswapV3Pool limit orders tests', () => {
 
     it('Creating limit orders should revert if tick is equal to current tick', async () => {
         // Should emit 'Mint' event
-        let amount = expandTo18Decimals(5);
+        let amount = expandTo18Decimals(5)
         let tickLower = (await pool.slot0()).tick
         await expect(pool.createLimitOrder(
-            await lpRecipient.getAddress(),
+            await lpRecipient1.getAddress(),
             tickLower,
             amount
         )).to.be.revertedWith('TL')
@@ -117,19 +118,19 @@ describe('UniswapV3Pool limit orders tests', () => {
 
     it('Create limit order and collect before fulfilling', async () => {
         // Create limit order
-        let amount = expandTo18Decimals(5);
+        let amount = expandTo18Decimals(5)
         let tickLower = (await pool.slot0()).tick + await pool.tickSpacing()
         await pool.createLimitOrder(
-            await lpRecipient.getAddress(),
+            await lpRecipient1.getAddress(),
             tickLower,
             amount
         )
 
-        await expect(pool.connect(lpRecipient).collectLimitOrder(
-            await lpRecipient.getAddress(),
+        await expect(pool.connect(lpRecipient1).collectLimitOrder(
+            await lpRecipient1.getAddress(),
             tickLower
         )).to.emit(pool, "Burn").withArgs(
-            await lpRecipient.getAddress(),
+            await lpRecipient1.getAddress(),
             tickLower,
             tickLower + await pool.tickSpacing(),
             amount,
@@ -140,10 +141,10 @@ describe('UniswapV3Pool limit orders tests', () => {
 
     it('Fulfill limit order by swapping tokens (greater tick)', async () => {
         // Create limit order
-        const amount = expandTo18Decimals(5);
+        const amount = expandTo18Decimals(5)
         const tickLower = (await pool.slot0()).tick + await pool.tickSpacing()
         await pool.createLimitOrder(
-            await lpRecipient.getAddress(),
+            await lpRecipient1.getAddress(),
             tickLower,
             amount
         )
@@ -158,22 +159,22 @@ describe('UniswapV3Pool limit orders tests', () => {
         )
 
         // Withdraw swapped tokens + fees
-        console.log(await token1.balanceOf(lpRecipient.address))
-        await pool.connect(lpRecipient).collectLimitOrder(
-            await lpRecipient.getAddress(),
+        console.log(await token1.balanceOf(lpRecipient1.address))
+        await pool.connect(lpRecipient1).collectLimitOrder(
+            await lpRecipient1.getAddress(),
             tickLower
         )
         // TODO: Compare the balances, difference should be equal to
         // swapped tokens + collected fees
-        console.log(await token1.balanceOf(lpRecipient.address))
+        console.log(await token1.balanceOf(lpRecipient1.address))
     })
 
     it('Fulfill limit order by swapping tokens (smaller tick)', async () => {
         // Create limit order
-        const amount = expandTo18Decimals(5);
+        const amount = expandTo18Decimals(5)
         const tickLower = (await pool.slot0()).tick - await pool.tickSpacing()
         await pool.createLimitOrder(
-            await lpRecipient.getAddress(),
+            await lpRecipient1.getAddress(),
             tickLower,
             amount
         )
@@ -188,14 +189,70 @@ describe('UniswapV3Pool limit orders tests', () => {
         )
 
         // Withdraw swapped tokens + fees
-        console.log(await token0.balanceOf(lpRecipient.address))
-        await pool.connect(lpRecipient).collectLimitOrder(
-            await lpRecipient.getAddress(),
+        console.log(await token0.balanceOf(lpRecipient1.address))
+        await pool.connect(lpRecipient1).collectLimitOrder(
+            await lpRecipient1.getAddress(),
             tickLower
         )
         // TODO: Compare the balances, difference should be equal to
         // swapped tokens + collected fees
-        console.log(await token0.balanceOf(lpRecipient.address))
+        console.log(await token0.balanceOf(lpRecipient1.address))
     })
+
+    it('Two users open limit orders (greater)', async () => {
+        // Create limit order 1
+        const amount1 = expandTo18Decimals(5)
+        const tickLower = (await pool.slot0()).tick + await pool.tickSpacing()
+        await pool.createLimitOrder(
+            await lpRecipient1.getAddress(),
+            tickLower,
+            amount1
+        )
+
+        // Create limit order 2
+        const amount2 = expandTo18Decimals(10)
+        await pool.createLimitOrder(
+            await lpRecipient2.getAddress(),
+            tickLower,
+            amount2
+        )
+
+        // swap tokens oneForZero
+        const amountIn = expandTo18Decimals(15);
+        await swapTarget.swapExact1For0(
+            pool.address,
+            amountIn,
+            await wallet.getAddress(),
+            MAX_SQRT_RATIO.sub(1)
+        )
+
+        // Withdraw 1 swapped tokens + fees
+        const balance11Before = await token1.balanceOf(lpRecipient1.address);
+        console.log(balance11Before)
+        await pool.connect(lpRecipient1).collectLimitOrder(
+            await lpRecipient1.getAddress(),
+            tickLower
+        )
+        // TODO: Compare the balances, difference should be equal to
+        // swapped tokens + collected fees
+        const balance11Diff = (await token1.balanceOf(lpRecipient1.address)).sub(balance11Before);
+        console.log(balance11Diff)
+
+        // Withdraw 2 swapped tokens + fees
+        const balance12Before = await token1.balanceOf(lpRecipient2.address);
+        console.log(balance12Before)
+        await pool.connect(lpRecipient2).collectLimitOrder(
+            await lpRecipient2.getAddress(),
+            tickLower
+        )
+        // TODO: Compare the balances, difference should be equal to
+        // swapped tokens + collected fees
+        const balance12Diff = (await token1.balanceOf(lpRecipient2.address)).sub(balance12Before);
+        console.log(balance12Diff)
+
+        // User1 balance should be less than User2
+        // since it has provided less liquidity
+        expect(balance11Diff.lt(balance12Diff)).to.be.true
+    });
 
 })
